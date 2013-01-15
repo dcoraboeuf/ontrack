63a6ea5580a8422df904fb13d06a16fd19e700e2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-15 18:30:38 +0100

    Starting with layout

--- ontrack-web/src/main/java/net/ontrack/web/config/WebConfig.java
+++ ontrack-web/src/main/java/net/ontrack/web/config/WebConfig.java
@@ -1,5 +1,46 @@
 package net.ontrack.web.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Bean
+	public FreeMarkerConfig freemarkerConfig() {
+		FreeMarkerConfigurer c = new FreeMarkerConfigurer();
+		c.setTemplateLoaderPath("/WEB-INF/views");
+		return c;
+	}
+	
+	/*
+	<!-- FIXME Uses profiles for caching -->
+	<bean id="viewResolver" class="org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver">
+		<property name="cache" value="false" />
+		<property name="prefix" value="" />
+		<property name="suffix" value=".html" />
+	</bean>
+	*/
+	@Bean
+	public ViewResolver viewResolver() {
+		FreeMarkerViewResolver o = new FreeMarkerViewResolver();
+		o.setCache(false);
+		o.setPrefix("");
+		o.setSuffix(".html");
+		return o;
+	}
 
 }
---
+++ ontrack-web/src/main/java/net/ontrack/web/home/HomeController.java
@@ -0,0 +1,16 @@
+package net.ontrack.web.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		// OK
+		return "home";
+	}
+
+}
