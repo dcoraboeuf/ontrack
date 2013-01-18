1e91ad0ff96a68ae557a09ce2e3c5ce8ca88bacb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-18 16:01:00 +0100

    Initialisation of the database at startup

---
+++ ontrack-backend/src/main/java/net/ontrack/backend/db/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package net.ontrack.backend.db;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import net.ontrack.core.RunProfile;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+@Configuration
+@Profile({RunProfile.IT, RunProfile.DEV, RunProfile.PROD})
+public class DataSourceConfig {
+	
+	@Bean
+	public DataSource dataSource() throws IllegalArgumentException, NamingException {
+		JndiObjectFactoryBean factory = new JndiObjectFactoryBean();
+		factory.setExpectedType(DataSource.class);
+		factory.setJndiName("jdbc/ontrack");
+		factory.afterPropertiesSet();
+		DataSource dataSource = (DataSource) factory.getObject();
+		return dataSource;
+	}
+
+}
