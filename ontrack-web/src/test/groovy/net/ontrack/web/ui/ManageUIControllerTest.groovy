1bc4dbb1d0bee5d1d9d0b4ee588b0435e2364ecc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-25 11:41:08 +0100

    MVC test OK

--- ontrack-web/src/test/groovy/net/ontrack/web/ui/ManageUIControllerTest.groovy
+++ ontrack-web/src/test/groovy/net/ontrack/web/ui/ManageUIControllerTest.groovy
@@ -19,24 +19,18 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 class ManageUIControllerTest extends AbstractWebTest {
 	
 	@Test
-	void http404() {
-		this.mockMvc.perform(get("/ui/404"))
-			.andDo(MockMvcResultHandlers.print());
-	}
-	
-	@Test
 	void createProjectGroup() {
 		this.mockMvc.perform(
-			post("/ui/manage/projectGroup")
+			post("/ui/manage/projectgroup")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json(new ProjectGroupCreationForm("GRP1", "My first group")))
 				.accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andDo(MockMvcResultHandlers.print());
-//			.andExpect(content().contentType("application/json"))
-//			.andExpect(jsonPath('$.id').value(greaterThan(0)))
-//			.andExpect(jsonPath('$.name').value("GRP1"))
-//			.andExpect(jsonPath('$.description').value("My first group"));
+			.andDo(MockMvcResultHandlers.print())
+			.andExpect(content().contentType("application/json;charset=UTF-8"))
+			.andExpect(jsonPath('$.id').value(greaterThan(0)))
+			.andExpect(jsonPath('$.name').value("GRP1"))
+			.andExpect(jsonPath('$.description').value("My first group"))
 	}
 
 }
