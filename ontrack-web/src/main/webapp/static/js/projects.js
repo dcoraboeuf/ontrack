c1f52e1957b830fe8a3e4f977a80eb8890823f0f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-01-30 17:08:28 +0100

    #2 Goes to the project page after creation

--- ontrack-web/src/main/webapp/static/js/projects.js
+++ ontrack-web/src/main/webapp/static/js/projects.js
@@ -7,8 +7,7 @@ var Projects = function () {
 			'POST',
 			'ui/manage/project',
 			function (data) {
-				Application.reload('projects');
-				Application.reload('audit');
+				location = 'gui/project/' + data.name;
 			});
 	}
 	
