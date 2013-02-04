0d27273448bd98e5f831becc974d765859e8b9b9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-04 21:39:29 +0100

    Creating a validation run

---
+++ ontrack-web/src/test/client/validation-run-create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT=$1
+BRANCH=$2
+BUILD=$3
+VALIDATION_STAMP=$4
+STATUS=$5
+DESCRIPTION=$6
+
+URL=http://localhost:8080/ontrack/ui/control/validation/${PROJECT}/${BRANCH}/${VALIDATION_STAMP}/${BUILD}
+DATA="{\"status\":\"${STATUS}\",\"description\":\"${DESCRIPTION}\"}"
+
+echo POST to $URL
+echo DATA is
+echo ${DATA}
+echo
+echo
+
+curl ${URL} --header "Content-Type: application/json" --data "$DATA"
