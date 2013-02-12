0c2936c9fc9f89d89e71d0baf63e2c06be5f8a45
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-12 22:07:59 +0100

    Reusing JS code

--- ontrack-web/src/main/webapp/static/js/builds.js
+++ ontrack-web/src/main/webapp/static/js/builds.js
@@ -1,4 +1,15 @@
 var Builds = function () {
+
+    function runs(buildValidationStamp) {
+        var html = '';
+        $.each(buildValidationStamp.runs, function (index, run) {
+            html += ' <p class="validation-run status-{0}">'.format(run.status);
+            html += '<a href="gui/validation_run/{0}"><i class="icon-play"></i> <span class="validation-run-status">{1}</span></a>'.format(run.runId, run.status.html());
+            html += ' <span class="validation-run-description">{0}</span>'.format(run.statusDescription.html());
+            html += '</p>';
+        });
+        return html;
+    }
 	
 	function buildTemplate (project, branch) {
 	    return function (branchBuilds) {
@@ -29,12 +40,7 @@ var Builds = function () {
                         html += '<td>';
                         if (buildValidationStamp) {
                             if (buildValidationStamp.run) {
-                                $.each(buildValidationStamp.runs, function (index, run) {
-                                    html += ' <p class="validation-run status-{0}">'.format(run.status);
-                                    html += '<a href="gui/validation_run/{0}"><i class="icon-play"></i> <span class="validation-run-status">{1}</span></a>'.format(run.runId, run.status.html());
-                                    html += ' <span class="validation-run-description">{0}</span>'.format(run.statusDescription.html());
-                                    html += '</p>';
-                                });
+                                html += runs(buildValidationStamp);
                             } else {
                                 html += '<span class="muted">{0}</span>'.format(loc('validationRun.notRun'));
                             }
@@ -62,12 +68,7 @@ var Builds = function () {
                 html += ValidationStamps.validationStampImage(project, branch, stamp);
                 html += ' <a href="gui/validation_stamp/{0}/{1}/{2}">{2}</a>'.format(project.html(), branch.html(), stamp.name.html());
                 if (stamp.run) {
-                    $.each(stamp.runs, function (index, run) {
-                        html += ' <p class="validation-run status-{0}">'.format(run.status);
-                        html += '<a href="gui/validation_run/{0}"><i class="icon-play"></i> <span class="validation-run-status">{1}</span></a>'.format(run.runId, run.status.html());
-                        html += ' <span class="validation-run-description">{0}</span>'.format(run.statusDescription.html());
-                        html += '</p>';
-                    });
+                    html += runs(stamp);
                 }
                 html += '</div>';
                 return html;
