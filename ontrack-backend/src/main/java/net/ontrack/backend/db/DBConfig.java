8900a01f750c3b0cc12582bf0c96cfd094447632
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-12 12:19:11 +0100

    Configuration table

--- ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 5;
+	public static final int VERSION = 6;
 	
 	@Autowired
 	private DataSource dataSource;
