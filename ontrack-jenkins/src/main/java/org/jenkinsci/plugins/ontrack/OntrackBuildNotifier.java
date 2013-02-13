79824fb8b3de52de6563470dc38f04d73a3ba6e3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-13 14:15:51 +0100

    Jenkins: expanding the parameters

--- ontrack-jenkins/src/main/java/org/jenkinsci/plugins/ontrack/OntrackBuildNotifier.java
+++ ontrack-jenkins/src/main/java/org/jenkinsci/plugins/ontrack/OntrackBuildNotifier.java
@@ -8,15 +8,20 @@ import hudson.model.BuildListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Publisher;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Allows to notify for a build.
  */
 public class OntrackBuildNotifier extends AbstractOntrackNotifier {
+
+    public static final String REGEX_ENV_VARIABLE = "\\$\\{([a-zA-Z0-9_]+)\\}";
 
     private final String project;
     private final String branch;
@@ -43,12 +48,48 @@ public class OntrackBuildNotifier extends AbstractOntrackNotifier {
 
     @Override
     public boolean perform(AbstractBuild<?, ?> theBuild, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-        // TODO Expands the expressions into actual values
+        // Expands the expressions into actual values
+        String projectName = expand(project, theBuild, listener);
+        String branchName = expand(branch, theBuild, listener);
+        String buildName= expand(build, theBuild, listener);
         // Logging of parameters
-        listener.getLogger().format("Creating build %s on project %s for branch %s%n", build, project, branch);
+        listener.getLogger().format("Creating build %s on project %s for branch %s%n", buildName, projectName, branchName);
         // FIXME Calling ontrack UI
         // OK
         return true;
+    }
+
+    protected String expand(String template, AbstractBuild<?, ?> theBuild, BuildListener listener) {
+        if (StringUtils.isBlank(template)) {
+            return template;
+        } else {
+            Pattern pattern = Pattern.compile(REGEX_ENV_VARIABLE);
+            Matcher matcher = pattern.matcher(template);
+            StringBuffer result = new StringBuffer();
+            while (matcher.find()) {
+                String name = matcher.group(1);
+                String value = getParameter(name, theBuild, listener);
+                if (value == null) {
+                    throw new IllegalStateException("Cannot find any replacement value for environment variable " + name);
+                }
+                matcher = matcher.appendReplacement(result, value);
+            }
+            matcher.appendTail(result);
+            return result.toString();
+        }
+    }
+
+    private String getParameter(String name, AbstractBuild<?, ?> theBuild, BuildListener listener) {
+        String value = (String) theBuild.getBuildVariableResolver().resolve(name);
+        if (value != null) {
+            return value;
+        } else {
+            try {
+                return theBuild.getEnvironment(listener).get(name);
+            } catch (Exception ex) {
+                throw new RuntimeException("Cannot get value for " + name, ex);
+            }
+        }
     }
 
     @Override
@@ -77,8 +118,6 @@ public class OntrackBuildNotifier extends AbstractOntrackNotifier {
         public boolean isApplicable(Class<? extends AbstractProject> jobType) {
             return true;
         }
-
-
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
