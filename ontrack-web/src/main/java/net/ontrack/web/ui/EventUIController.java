4d9f060142c9a960667188dec64f0ed3d89b0e62
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-14 13:33:57 +0100

    Jenkins: login into ontrack

--- ontrack-web/src/main/java/net/ontrack/web/ui/EventUIController.java
+++ ontrack-web/src/main/java/net/ontrack/web/ui/EventUIController.java
@@ -1,7 +1,6 @@
 package net.ontrack.web.ui;
 
-import java.util.List;
-
+import net.ontrack.core.model.Ack;
 import net.ontrack.core.model.EventFilter;
 import net.ontrack.core.model.ExpandedEvent;
 import net.ontrack.core.ui.EventUI;
@@ -9,13 +8,14 @@ import net.ontrack.service.EventService;
 import net.ontrack.web.support.AbstractUIController;
 import net.ontrack.web.support.ErrorHandler;
 import net.sf.jstring.Strings;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class EventUIController extends AbstractUIController implements EventUI {
@@ -27,6 +27,15 @@ public class EventUIController extends AbstractUIController implements EventUI {
 		super(errorHandler, strings);
 		this.auditService = auditService;
 	}
+
+    /**
+     * Forces the login of the user for pure-UI calls.
+     */
+    @RequestMapping(value = "/ui/login", method = RequestMethod.GET)
+    public @ResponseBody
+    Ack login() {
+        return Ack.OK;
+    }
 	
 	@Override
 	@RequestMapping(value = "/ui/event", method = RequestMethod.GET)
