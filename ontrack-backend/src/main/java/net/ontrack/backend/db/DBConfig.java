bf19d82c4489edaef255320e92c0a866db8f2fc4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-21 21:11:16 +0100

    Run order

--- ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 8;
+	public static final int VERSION = 9;
 	
 	@Autowired
 	private DataSource dataSource;
