05463e0f1929188d370ab3f201c536073700605c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-02-26 16:36:03 +0100

    Promotion levels management: droppable zones (wip)

--- ontrack-web/src/main/webapp/static/js/promotion-level-management.js
+++ ontrack-web/src/main/webapp/static/js/promotion-level-management.js
@@ -1,8 +1,21 @@
 var PromotionLevelManagement = function () {
 
     function init () {
+        // Free validation stamps are draggable
         $('span.validationStamp.free').draggable({
-            revert: true
+            revert: "invalid"
+        });
+        // Initialization
+        // Droppable zones for the promotion levels
+        $('.promotionLevelStamps').droppable({
+            activeClass: "ui-state-hover",
+            hoverClass: "ui-state-active",
+            drop: function( event, ui ) {
+            $( this )
+                .addClass( "ui-state-highlight" )
+                .find( "p" )
+                .html( "Dropped!" );
+         }
         });
     }
 
