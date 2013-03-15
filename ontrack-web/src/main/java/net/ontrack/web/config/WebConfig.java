afe766e4c1d1e558c41e505465c963366e637211
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-15 21:46:05 +0100

    Formatting

--- ontrack-web/src/main/java/net/ontrack/web/config/WebConfig.java
+++ ontrack-web/src/main/java/net/ontrack/web/config/WebConfig.java
@@ -46,8 +46,8 @@ import java.util.*;
 @PropertySource("/META-INF/strings/core.properties")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
     @Autowired
     private SecurityUtils securityUtils;
@@ -55,58 +55,58 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private PropertiesService propertiesService;
 
-	// TODO Moves this to the core
-	@Bean
-	public Strings strings() {
-		return StringsLoader.auto(Locale.ENGLISH, Locale.FRENCH);
-	}
-	
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.clear();
-		// Plain text
-		converters.add(new StringHttpMessageConverter());
-		// JSON
-		MappingJacksonHttpMessageConverter mapper = new MappingJacksonHttpMessageConverter();
-		mapper.setObjectMapper(jacksonObjectMapper());
-		converters.add(mapper);
-	}
+    // TODO Moves this to the core
+    @Bean
+    public Strings strings() {
+        return StringsLoader.auto(Locale.ENGLISH, Locale.FRENCH);
+    }
 
-	@Bean
-	public Object exporter() throws IOException {
-		MBeanExporter exporter = new MBeanExporter();
-		exporter.setBeans(Collections.<String, Object> singletonMap("bean:name=strings", strings()));
-		return exporter;
-	}
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        // Plain text
+        converters.add(new StringHttpMessageConverter());
+        // JSON
+        MappingJacksonHttpMessageConverter mapper = new MappingJacksonHttpMessageConverter();
+        mapper.setObjectMapper(jacksonObjectMapper());
+        converters.add(mapper);
+    }
 
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
+    @Bean
+    public Object exporter() throws IOException {
+        MBeanExporter exporter = new MBeanExporter();
+        exporter.setBeans(Collections.<String, Object>singletonMap("bean:name=strings", strings()));
+        return exporter;
+    }
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String version = env.getProperty("app.version");
-		registry.addResourceHandler(String.format("/resources/v%s/**", version)).addResourceLocations("/static/");
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String version = env.getProperty("app.version");
+        registry.addResourceHandler(String.format("/resources/v%s/**", version)).addResourceLocations("/static/");
         registry.addResourceHandler("/extension/**").addResourceLocations("classpath:/META-INF/extension/");
-	}
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LocaleInterceptor(strings()));
-		registry.addInterceptor(new WebInterceptor());
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LocaleInterceptor(strings()));
+        registry.addInterceptor(new WebInterceptor());
+    }
 
-	@Bean
-	public FreeMarkerConfig freemarkerConfig() {
-		FreeMarkerConfigurer c = new FreeMarkerConfigurer();
-		c.setTemplateLoaderPath("/WEB-INF/views");
-		// Freemarker variables
-		Map<String, Object> variables = new HashMap<>();
-		variables.put("loc", new FnLoc(strings()));
-		variables.put("locSelected", new FnLocSelected());
-		variables.put("locFormatDate", new FnLocFormatDate(strings()));
-		variables.put("locFormatTime", new FnLocFormatTime(strings()));
+    @Bean
+    public FreeMarkerConfig freemarkerConfig() {
+        FreeMarkerConfigurer c = new FreeMarkerConfigurer();
+        c.setTemplateLoaderPath("/WEB-INF/views");
+        // Freemarker variables
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("loc", new FnLoc(strings()));
+        variables.put("locSelected", new FnLocSelected());
+        variables.put("locFormatDate", new FnLocFormatDate(strings()));
+        variables.put("locFormatTime", new FnLocFormatTime(strings()));
         variables.put("secLogged", new FnSecLogged(securityUtils));
         variables.put("secAdmin", new FnSecAdmin(securityUtils));
         variables.put("secDisplayName", new FnSecDisplayName(securityUtils));
@@ -114,63 +114,63 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         variables.put("propertyDisplay", new FnPropertyDisplay(strings(), propertiesService));
         // OK
         c.setFreemarkerVariables(variables);
-		// OK
-		return c;
-	}
+        // OK
+        return c;
+    }
 
-	@Bean
-	public ViewResolver freemarkerViewResolver() {
-		FreeMarkerViewResolver o = new FreeMarkerViewResolver();
-		o.setCache(false);
-		o.setPrefix("");
-		o.setSuffix(".html");
-		return o;
-	}
+    @Bean
+    public ViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver o = new FreeMarkerViewResolver();
+        o.setCache(false);
+        o.setPrefix("");
+        o.setSuffix(".html");
+        return o;
+    }
 
-	@Bean
-	public View jsonViewResolver() {
-		MappingJacksonJsonView o = new MappingJacksonJsonView();
-		o.setObjectMapper(jacksonObjectMapper());
-		return o;
-	}
-	
-	@Bean
-	public CommonsMultipartResolver multipartResolver() {
-		CommonsMultipartResolver o = new CommonsMultipartResolver();
-		o.setMaxUploadSize(4 * 1024); // 4K limit 
-		return o;
-	}
-	
-	@Bean
-	public ObjectMapper jacksonObjectMapper() {
-		ObjectMapper mapper = ObjectMapperFactory.createObjectMapper();
-		
-		jsonJoda(mapper);
-		
-		return mapper;
-	}
+    @Bean
+    public View jsonViewResolver() {
+        MappingJacksonJsonView o = new MappingJacksonJsonView();
+        o.setObjectMapper(jacksonObjectMapper());
+        return o;
+    }
 
-	protected void jsonJoda(ObjectMapper mapper) {
-		SimpleModule jodaModule = new SimpleModule("JodaTimeModule", new Version(1, 0, 0, null));
-		jsonLocalDateTime(jodaModule);
-		jsonLocalDate(jodaModule);
-		jsonLocalTime(jodaModule);
-		mapper.registerModule(jodaModule);
-	}
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver o = new CommonsMultipartResolver();
+        o.setMaxUploadSize(4 * 1024); // 4K limit
+        return o;
+    }
 
-	protected void jsonLocalDateTime(SimpleModule jodaModule) {
-		jodaModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
-		jodaModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
-	}
+    @Bean
+    public ObjectMapper jacksonObjectMapper() {
+        ObjectMapper mapper = ObjectMapperFactory.createObjectMapper();
 
-	protected void jsonLocalTime(SimpleModule jodaModule) {
-		jodaModule.addSerializer(LocalTime.class, new LocalTimeSerializer());
-		jodaModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer());
-	}
+        jsonJoda(mapper);
 
-	protected void jsonLocalDate(SimpleModule jodaModule) {
-		jodaModule.addSerializer(LocalDate.class, new LocalDateSerializer());
-		jodaModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
-	}
+        return mapper;
+    }
+
+    protected void jsonJoda(ObjectMapper mapper) {
+        SimpleModule jodaModule = new SimpleModule("JodaTimeModule", new Version(1, 0, 0, null));
+        jsonLocalDateTime(jodaModule);
+        jsonLocalDate(jodaModule);
+        jsonLocalTime(jodaModule);
+        mapper.registerModule(jodaModule);
+    }
+
+    protected void jsonLocalDateTime(SimpleModule jodaModule) {
+        jodaModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        jodaModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+    }
+
+    protected void jsonLocalTime(SimpleModule jodaModule) {
+        jodaModule.addSerializer(LocalTime.class, new LocalTimeSerializer());
+        jodaModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer());
+    }
+
+    protected void jsonLocalDate(SimpleModule jodaModule) {
+        jodaModule.addSerializer(LocalDate.class, new LocalDateSerializer());
+        jodaModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+    }
 
 }
