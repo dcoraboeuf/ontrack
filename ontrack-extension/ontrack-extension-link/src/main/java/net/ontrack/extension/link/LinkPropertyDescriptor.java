a3f8109e31c9d25c21b7b07774e0e35da82e2e2e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-18 19:32:52 +0100

    #49 Link property

---
+++ ontrack-extension/ontrack-extension-link/src/main/java/net/ontrack/extension/link/LinkPropertyDescriptor.java
@@ -0,0 +1,30 @@
+package net.ontrack.extension.link;
+
+import net.ontrack.core.model.Entity;
+import net.ontrack.extension.api.support.AbstractLinkPropertyExtensionDescriptor;
+import org.springframework.stereotype.Component;
+
+import java.util.EnumSet;
+
+@Component
+public class LinkPropertyDescriptor extends AbstractLinkPropertyExtensionDescriptor {
+
+    public LinkPropertyDescriptor() {
+        super("link", "link.png");
+    }
+
+    @Override
+    public EnumSet<Entity> getScope() {
+        return EnumSet.allOf(Entity.class);
+    }
+
+    @Override
+    public String getExtension() {
+        return "link";
+    }
+
+    @Override
+    public String getName() {
+        return "url";
+    }
+}
