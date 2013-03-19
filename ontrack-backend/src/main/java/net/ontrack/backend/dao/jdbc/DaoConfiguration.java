3324e6d259f066ead8101fe7a9829c191ae7b15d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-19 15:54:17 +0100

    #44 EHCache configuration

--- ontrack-backend/src/main/java/net/ontrack/backend/dao/jdbc/DaoConfiguration.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/dao/jdbc/DaoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableCaching
@@ -21,6 +22,7 @@ public class DaoConfiguration {
     @Bean
     public EhCacheManagerFactoryBean ehcache() {
         EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+        factoryBean.setConfigLocation(new ClassPathResource("/META-INF/dao/ehcache.xml"));
         return factoryBean;
     }
 
