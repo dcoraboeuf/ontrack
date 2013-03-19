4389eb0eeeddc925f091a0ed5faac77fb83bd62f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-19 20:50:23 +0100

    #59 Audit

--- ontrack-web/src/main/webapp/static/js/audit.js
+++ ontrack-web/src/main/webapp/static/js/audit.js
@@ -1,23 +1,12 @@
 var Audit = function () {
 	
-	function audit (item) {
-		var html = '<div class="{0}">'.format(item.status!="" ? 'status-' + item.status : '');
-		if (item.icon != "") {
-			html += '<img width="24" src="{0}" class="event-icon" /> '.format(item.icon.html());
-		}
-		html += item.html;
-		html += ' <span class="elapsed">- <span title="{1}">{0}</span></span>'.format(item.elapsed.html(), item.timestamp.html());
-		html += '</div>';
-		return html;
-	}
-	
 	return {
 		auditTemplate: function (filter) {
 		    return Template.config({
 		        url: 'gui/event?u=1' + filter,
 		        more: true,
 		        refresh: true,
-		        render: Template.asTable(audit)
+		        render: Template.asTableTemplate('auditTemplate')
 		    });
 		}
 	};
