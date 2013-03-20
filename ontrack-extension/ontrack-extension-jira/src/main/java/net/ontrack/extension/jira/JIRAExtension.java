905ca5bbb4560f441fb5bf45a51db35cdf3145c8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-20 10:31:52 +0100

    #41 JIRA issue property

---
+++ ontrack-extension/ontrack-extension-jira/src/main/java/net/ontrack/extension/jira/JIRAExtension.java
@@ -0,0 +1,30 @@
+package net.ontrack.extension.jira;
+
+import net.ontrack.extension.api.configuration.ConfigurationExtension;
+import net.ontrack.extension.api.property.PropertyExtensionDescriptor;
+import net.ontrack.extension.api.support.ExtensionAdapter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class JIRAExtension extends ExtensionAdapter {
+
+    public static final String EXTENSION = "jira";
+
+    public JIRAExtension() {
+        super(EXTENSION);
+    }
+
+    @Override
+    public List<? extends PropertyExtensionDescriptor> getPropertyExtensionDescriptors() {
+        return Collections.singletonList(new JIRAIssuePropertyExtension());
+    }
+
+    @Override
+    public List<? extends ConfigurationExtension> getConfigurationExtensions() {
+        // FIXME Implement net.ontrack.extension.jira.JIRAExtension.getConfigurationExtensions
+        return super.getConfigurationExtensions();
+    }
+}
