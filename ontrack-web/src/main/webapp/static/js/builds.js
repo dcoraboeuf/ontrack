1cd3f402ab6693e8fb9b8909afd3fa21974fa725
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-03-22 12:35:22 +0100

    #65 Template for the runs of a build

--- ontrack-web/src/main/webapp/static/js/builds.js
+++ ontrack-web/src/main/webapp/static/js/builds.js
@@ -1,47 +1,5 @@
 var Builds = function () {
 
-    function runs(project, branch, build, buildValidationStamp, compact) {
-        var html = '';
-        if (compact) {
-            // Run?
-            if (buildValidationStamp.run) {
-                // Gets the first run only
-                var run = buildValidationStamp.runs[0];
-                // Status 'ball'
-                html += '<a class="tooltip-source" data-toggle="tooltip" href="gui/project/{0}/branch/{1}/build/{2}/validation_stamp/{3}/validation_run/{4}" title="{8} - {6} - {7}"><img width="24" src="static/images/status-{5}.png" /></a>'.format(
-                    project.html(), // 0
-                    branch.html(), // 1
-                    build.html(), // 2
-                    buildValidationStamp.name.html(), // 3
-                    run.runOrder, // 4
-                    run.status.html(), // 5
-                    run.signature.elapsedTime, // 6
-                    run.signature.formattedTime, // 7
-                    run.statusDescription.html() // 8
-                );
-            } else {
-                html += '<img class="tooltip-source" width="24" height="24" src="static/images/status-NONE.png" title="{0}" />'.format(loc('validationRun.notRun'));
-            }
-        } else {
-            $.each(buildValidationStamp.runs, function (index, run) {
-                html += ' <p class="validation-run status-{0}">'.format(run.status);
-                html += '<a class="tooltip-source" href="gui/project/{0}/branch/{1}/build/{2}/validation_stamp/{3}/validation_run/{4}" title="{8} - {6} - {7}"><i class="icon-play"></i> <span class="validation-run-status">{5}</span></a>'
-                    .format(
-                        project.html(), // 0
-                        branch.html(), // 1
-                        build.html(), // 2
-                        buildValidationStamp.name.html(), // 3
-                        run.runOrder, // 4
-                        run.status.html(), // 5
-                        run.signature.elapsedTime, // 6
-                        run.signature.formattedTime, // 7
-                        run.statusDescription.html()); // 8
-                html += '</p>';
-            });
-        }
-        return html;
-    }
-
     function generateTableBuildRows (project, branch, branchBuilds) {
         Handlebars.registerHelper('promotionLevelsFn', function(options) {
             return generateBuildPromotionLevels(project,branch)(this.promotionLevels);
@@ -162,18 +120,12 @@ var Builds = function () {
 	        url: 'ui/manage/project/{0}/branch/{1}/build/{2}/validationStamps'.format(project, branch, build),
 	        refresh: true,
 	        render: Template.asTable(function (stamp) {
-                var pClass;
-                if (!stamp.run) {
-                    pClass = 'validation-stamp-norun';
-                }
-                var html = '<tr><td><div class="{0}">'.format(pClass);
-                html += ValidationStamps.validationStampImage(project, branch, stamp);
-                html += ' <a class="tooltip-source" href="gui/project/{0}/branch/{1}/validation_stamp/{2}">{2}</a>'.format(project.html(), branch.html(), stamp.name.html());
-                if (stamp.run) {
-                    html += runs(project, branch, build, stamp, false);
-                }
-                html += '</div></td></tr>';
-                return html;
+	            return Template.render('buildValidationStampsTemplate', {
+                    project: project,
+                    branch: branch,
+                    build: build,
+                    stamp: stamp
+	            });
 	        }),
 	        postRenderFn: Application.tooltips
 	    });
