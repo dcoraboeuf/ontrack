dfac94ef918924290bebbf187b8081fed56ed988
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-04-09 09:47:00 +0200

    #42 JS skeleton for loading the revisions

--- ontrack-extension/ontrack-extension-svnexplorer/src/main/resources/META-INF/extension/svnexplorer.changelog.js
+++ ontrack-extension/ontrack-extension-svnexplorer/src/main/resources/META-INF/extension/svnexplorer.changelog.js
@@ -1,7 +1,21 @@
 var ChangeLog = function () {
 
-    return {
+    var revisions = null;
 
+    function loadRevisions () {
+        if (revisions == null) {
+            // FIXME Loads the revisions
+        }
+    }
+
+    function init () {
+        $('#revisions').on('show', loadRevisions);
+    }
+
+    return {
+        init: init
     };
 
 } ();
+
+$(document).ready(ChangeLog.init);
