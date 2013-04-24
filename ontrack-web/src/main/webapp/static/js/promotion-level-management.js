a4a3c683fa16b9b6905ba15db8f8fb4d67d9a9cc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-04-24 13:37:50 +0200

    #32 Autopromotion AJAX call

--- ontrack-web/src/main/webapp/static/js/promotion-level-management.js
+++ ontrack-web/src/main/webapp/static/js/promotion-level-management.js
@@ -114,6 +114,42 @@ var PromotionLevelManagement = function () {
         });
     }
 
+    function autoPromote (button) {
+        // Gets the promotion level
+        var project = $('#project').val();
+        var branch = $('#branch').val();
+        var promotionLevel = $(button).attr('data-promotionLevel');
+        // Current state of the autopromotion
+        var currentAutoPromote = $(button).attr('data-autoPromote');
+        // URL
+        var url = 'ui/manage/project/{0}/branch/{1}/promotion_level/{2}/autopromote'.format(
+            project.html(),
+            branch.html(),
+            promotionLevel.html()
+        );
+        if (currentAutoPromote == 'true') {
+            url += '/unset';
+        } else {
+            url += '/set';
+        }
+        // Call
+        AJAX.put({
+            url: url,
+            loading: {
+                el: button
+            },
+            successFn: function (flag) {
+                if (flag.set) {
+                    $(button).attr('data-autoPromote', 'true');
+                    $(button).text(loc('promotion_level.management.notauto'));
+                } else {
+                    $(button).attr('data-autoPromote', 'false');
+                    $(button).text(loc('promotion_level.management.auto'));
+                }
+            }
+        });
+    }
+
     function initDropZones () {
         $('.promotionLevelStamps').each(function (index, promotionLevelItem) {
             var dropZone = $('#dropzone-label-' + $(promotionLevelItem).attr('data-promotionLevel'));
@@ -160,7 +196,8 @@ var PromotionLevelManagement = function () {
 
         init: init,
         up: up,
-        down: down
+        down: down,
+        autoPromote: autoPromote
 
     };
 
