235edb0e0e1a196a4e6e769905fc99e14dee350b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-04-26 19:36:52 +0200

    #89 AJAX call

---
+++ ontrack-web/src/main/webapp/static/js/validation_stamp.filter.js
@@ -0,0 +1,37 @@
+var ValidationStampFilter = function () {
+
+    function toggleFilter (chk) {
+        var filtered = ($(chk).attr('filtered') == 'true');
+        var project = $('#project').val();
+        var branch = $('#branch').val();
+        var stamp = $(chk).attr('stamp');
+        AJAX.call({
+            url: 'ui/admin/project/{0}/branch/{1}/validation_stamp/{2}/filter'.format(
+                project,
+                branch,
+                stamp
+            ),
+            method: (filtered ? 'DELETE' : 'PUT'),
+            loading: {
+                el: $('stamp_{0}'.format(stamp))
+            },
+            successFn: function (ack) {
+                if (ack.success) {
+                    filtered = !filtered;
+                    if (filtered) {
+                        $('stamp_line_{0}'.format(stamp)).addClass('filtered');
+                        chk.attr('filtered', 'true');
+                    } else {
+                        $('stamp_line_{0}'.format(stamp)).removeClass('filtered');
+                        chk.attr('filtered', 'false');
+                    }
+                }
+            }
+        });
+    }
+
+    return {
+        toggleFilter: toggleFilter
+    };
+
+} ();
