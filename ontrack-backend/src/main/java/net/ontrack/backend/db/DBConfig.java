244cb87e95c69a1bef266be8b2dc91d68c1b76f4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-04-29 20:41:18 +0200

    #134 Dropped the `project_group` table

--- ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 21;
+	public static final int VERSION = 22;
 	
 	@SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
