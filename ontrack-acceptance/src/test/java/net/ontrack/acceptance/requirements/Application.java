07ea3f2088a8e2813c803de3deec3f78add03af6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-09 21:21:47 +0200

    Acceptance - project

---
+++ ontrack-acceptance/src/test/java/net/ontrack/acceptance/requirements/Application.java
@@ -0,0 +1,11 @@
+package net.ontrack.acceptance.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Search {
+        public class SearchByKeyword {}
+        public class SearchByMultipleKeywords {}
+    }
+}
---
+++ ontrack-acceptance/src/test/java/net/ontrack/acceptance/steps/EndUserSteps.java
@@ -0,0 +1,44 @@
+package net.ontrack.acceptance.steps;
+
+import net.ontrack.acceptance.pages.DictionaryPage;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.steps.ScenarioSteps;
+
+import static ch.lambdaj.Lambda.join;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class EndUserSteps extends ScenarioSteps {
+
+    DictionaryPage dictionaryPage;
+
+    public EndUserSteps(Pages pages) {
+        super(pages);
+    }
+
+    @Step
+    public void enters(String keyword) {
+        dictionaryPage.enter_keywords(keyword);
+    }
+
+    @Step
+    public void starts_search() {
+        dictionaryPage.lookup_terms();
+    }
+
+    @Step
+    public void should_see_definition(String definition) {
+        assertThat(dictionaryPage.getDefinitions()).contains(definition);
+    }
+
+    @Step
+    public void is_the_home_page() {
+        dictionaryPage.open();
+    }
+
+    @Step
+    public void looks_for(String term) {
+        enters(term);
+        starts_search();
+    }
+}
