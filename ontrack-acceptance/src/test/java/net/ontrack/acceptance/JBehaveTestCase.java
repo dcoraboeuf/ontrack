551d850dd86de6ab7f3e7a04e718dff4c97c346b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-09 21:57:26 +0200

    Acceptance: JBehave mode

---
+++ ontrack-acceptance/src/test/java/net/ontrack/acceptance/JBehaveTestCase.java
@@ -0,0 +1,6 @@
+package net.ontrack.acceptance;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class JBehaveTestCase extends ThucydidesJUnitStories {
+}
---
+++ ontrack-acceptance/src/test/resources/stories/management/project/exploring_projects.story
@@ -0,0 +1,6 @@
+Story: Exploring the projects
+
+Scenario: List of projects on the home page
+
+Given I am on the home page
+Then I see the TEST1 project
