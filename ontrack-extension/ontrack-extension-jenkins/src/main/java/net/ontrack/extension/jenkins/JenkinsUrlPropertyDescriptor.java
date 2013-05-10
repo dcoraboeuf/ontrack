4fdce8ae53ccb3a2cdc6a766cc4247734782f615
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-10 11:21:40 +0200

    #90 Jenkins URL is editable by administrators on branches and validation stamps

--- ontrack-extension/ontrack-extension-jenkins/src/main/java/net/ontrack/extension/jenkins/JenkinsUrlPropertyDescriptor.java
+++ ontrack-extension/ontrack-extension-jenkins/src/main/java/net/ontrack/extension/jenkins/JenkinsUrlPropertyDescriptor.java
@@ -1,6 +1,7 @@
 package net.ontrack.extension.jenkins;
 
 import net.ontrack.core.model.Entity;
+import net.ontrack.core.security.SecurityRoles;
 import net.ontrack.extension.api.property.AbstractLinkPropertyExtensionDescriptor;
 import org.springframework.stereotype.Component;
 
@@ -26,5 +27,19 @@ public class JenkinsUrlPropertyDescriptor extends AbstractLinkPropertyExtensionD
     @Override
     public String getName() {
         return "url";
+    }
+
+    /**
+     * Editable only by administrators on branches & validation stamps
+     */
+    @Override
+    public String getRoleForEdition(Entity entity) {
+        switch (entity) {
+            case BRANCH:
+            case VALIDATION_STAMP:
+                return SecurityRoles.ADMINISTRATOR;
+            default:
+                return super.getRoleForEdition(entity);
+        }
     }
 }
