88a7b47b2135bc46638d46d1036428086b4673aa
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-16 19:10:39 +0200

    #157 Sending the validation stamps replacements

--- ontrack-web/src/main/webapp/static/js/branchClone.js
+++ ontrack-web/src/main/webapp/static/js/branchClone.js
@@ -13,7 +13,22 @@ var BranchClone = function () {
                 value: value
             });
         });
-        // TODO Collects the validation stamp expressions
+        // Collects the validation stamp expressions
+        var validationStampExpressions = [];
+        $('.validation-stamp-property').each(function (index, el) {
+            var extension = $(el).attr('extension');
+            var property = $(el).attr('property');
+            var regex = $('#validation-stamp-{0}-{1}-regex'.format(extension, property)).val();
+            var replacement = $('#validation-stamp-{0}-{1}-replacement'.format(extension, property)).val();
+            if (regex != '') {
+                validationStampExpressions.push({
+                    extension: extension,
+                    property: property,
+                    regex: regex,
+                    replacement: replacement
+                });
+            }
+        });
         // TODO Collects the promotion level expressions
         // AJAX call
         AJAX.post({
@@ -24,7 +39,8 @@ var BranchClone = function () {
             data: {
                 name: $('#name').val(),
                 description: $('#description').val(),
-                branchProperties: branchProperties
+                branchProperties: branchProperties,
+                validationStampExpressions: validationStampExpressions
             },
             successFn: function (summary) {
                 location = 'gui/project/{0}/branch/{1}'.format(project, summary.name);
