d440edc4f0f9816cce3a8c9b943db99867bf7b0a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-16 19:06:16 +0200

    #157 Sending the branch properties

--- ontrack-web/src/main/webapp/static/js/branchClone.js
+++ ontrack-web/src/main/webapp/static/js/branchClone.js
@@ -1,6 +1,21 @@
 var BranchClone = function () {
 
     function clone (project, branch) {
+        // Collects the branch properties
+        var branchProperties = [];
+        $('.branch-property').each (function (index, el) {
+            var extension = $(el).attr('extension');
+            var property = $(el).attr('property');
+            var value = $('#extension-{0}-{1}'.format(extension, property)).val();
+            branchProperties.push({
+                extension: extension,
+                property: property,
+                value: value
+            });
+        });
+        // TODO Collects the validation stamp expressions
+        // TODO Collects the promotion level expressions
+        // AJAX call
         AJAX.post({
             url: 'ui/manage/project/{0}/branch/{1}/clone'.format(project, branch),
             loading: {
@@ -8,7 +23,8 @@ var BranchClone = function () {
             },
             data: {
                 name: $('#name').val(),
-                description: $('#description').val()
+                description: $('#description').val(),
+                branchProperties: branchProperties
             },
             successFn: function (summary) {
                 location = 'gui/project/{0}/branch/{1}'.format(project, summary.name);
