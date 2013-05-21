cbcee99d7c19b6fd0843a2fe56996ce697cad353
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-21 17:04:32 +0200

    #156 Being able to run one story only

--- ontrack-acceptance/src/test/java/net/ontrack/acceptance/JBehaveTestCase.java
+++ ontrack-acceptance/src/test/java/net/ontrack/acceptance/JBehaveTestCase.java
@@ -1,6 +1,16 @@
 package net.ontrack.acceptance;
 
 import net.thucydides.jbehave.ThucydidesJUnitStories;
+import org.apache.commons.lang3.StringUtils;
 
 public class JBehaveTestCase extends ThucydidesJUnitStories {
+
+    public JBehaveTestCase() {
+        super();
+        // Story pattern?
+        String storyNames = System.getProperty("story.name");
+        if (StringUtils.isNotBlank(storyNames)) {
+            findStoriesCalled(storyNames);
+        }
+    }
 }
