6b27119df8707055cf81cbd7798cf7dcc01d5e0c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-24 19:35:59 +0200

    requirejs: error section

--- ontrack-web/src/main/webapp/static/js/lib/dialog.js
+++ ontrack-web/src/main/webapp/static/js/lib/dialog.js
@@ -14,8 +14,21 @@ define(['config', 'render', 'jquery'], function(config, render, $) {
             var data = {};
             // Gets the HTML to display
             var html = $(compiledTemplate(data));
-            // TODO Error section
-            // TODO Button section
+            // Form
+            var form;
+            if ($(html)[0].tagName.toLowerCase() == 'form') {
+                form = html;
+            } else {
+                form = $(html)._findPos('form');
+            }
+            // Customization
+            if (form) {
+                // Error section
+                $('<div></div>')
+                    .addClass('error').addClass('hidden').addClass('alert').addClass('alert-error')
+                    .appendTo(form);
+                // TODO Button section
+            }
             // TODO Initialization of the content
             // Displays the dialog
             $(html).dialog({
