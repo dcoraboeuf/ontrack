12f5b426169af36598c332ef6372e8af57cf5fee
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-24 20:15:54 +0200

    requirejs: dialog cancel button

--- ontrack-web/src/main/webapp/static/js/lib/dialog.js
+++ ontrack-web/src/main/webapp/static/js/lib/dialog.js
@@ -1,13 +1,31 @@
 define(['config', 'render', 'jquery'], function(config, render, $) {
 
+    function createButton(dialog, button) {
+        var item = $('<button/>');
+        item.addClass('btn');
+        item.text(button.text);
+        if (button.action == 'submit') {
+            item.addClass('btn-primary').attr('type', 'submit');
+        } else if (button.action == 'cancel') {
+            item.addClass('btn-link').attr('type', 'button');
+            item.unbind('click');
+            item.click(dialog.closeFn);
+        } else {
+            item.attr('type', 'button');
+        }
+        return item;
+    }
+
     function show (dialog) {
         // Default configuration
         dialog = $.extend({
             width: 550,
             buttons:[{
-                text: 'general.submit'.loc()
+                text: 'general.submit'.loc(),
+                action: 'submit'
             }, {
-                text: 'general.cancel'.loc()
+                text: 'general.cancel'.loc(),
+                action: 'cancel'
             }]
         }, dialog);
         // Checks parameters
@@ -19,12 +37,22 @@ define(['config', 'render', 'jquery'], function(config, render, $) {
             var data = {};
             // Gets the HTML to display
             var html = $(compiledTemplate(data));
+            // ID
+            var dialogId = 'dialog-{0}'.format(dialog.templateId);
+            dialog.id = dialogId;
+            $(html).attr('id', dialogId);
+            // Close function
+            dialog.closeFn = function () {
+                $('#' + dialog.id).dialog('close');
+                $('#' + dialog.id).dialog('destroy');
+                $('#' + dialog.id).remove();
+            };
             // Form
             var form;
             if ($(html)[0].tagName.toLowerCase() == 'form') {
                 form = html;
             } else {
-                form = $(html)._findPos('form');
+                form = $(html).find('form');
             }
             // Customization
             if (form) {
@@ -32,7 +60,15 @@ define(['config', 'render', 'jquery'], function(config, render, $) {
                 $('<div></div>')
                     .addClass('error').addClass('hidden').addClass('alert').addClass('alert-error')
                     .appendTo(form);
-                // TODO Button section
+                // Button section
+                if (dialog.buttons && dialog.buttons.length > 0) {
+                    var controls = $('<div></div>').addClass('controls');
+                    $.each(dialog.buttons, function (index, button) {
+                        var item = createButton(dialog, button);
+                        controls.append(item);
+                    });
+                    $('<div></div>').addClass('control-group').append(controls).appendTo(form);
+                }
             }
             // TODO Initialization of the content
             // Displays the dialog
