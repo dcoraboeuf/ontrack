a293aea4437e9227d7ac4ffe74a6f491f14caf1a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-28 19:37:40 +0200

    requirejs: validation stamp - comments

--- ontrack-web/src/main/webapp/static/js/app/controller/validation-stamp-comments.js
+++ ontrack-web/src/main/webapp/static/js/app/controller/validation-stamp-comments.js
@@ -3,7 +3,8 @@ define(['render'], function (render) {
     return {
         url: function (config) {
             return 'ui/manage/project/{0}/branch/{1}/validation_stamp/{2}/comment?u=1'.format(config.project, config.branch, config.validation_stamp)
-        }
+        },
+        render: render.asTableTemplate('validation-stamp-comments')
     }
 
 });
