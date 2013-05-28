fb96a5c379c6e1c22290ff88bff9ed9afc982afa
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-05-28 19:54:40 +0200

    requirejs: validation stamp - update

--- ontrack-web/src/main/webapp/static/js/app/validation_stamp.js
+++ ontrack-web/src/main/webapp/static/js/app/validation_stamp.js
@@ -4,19 +4,53 @@ define(['jquery', 'ajax', 'dialog', 'application', 'dynamic'], function ($, ajax
     var branch = $('#branch').val();
     var validationStamp = $('#validation_stamp').val();
 
+    // Updating the validation stamp
+    function updateValidationStamp() {
+        ajax.get({
+            url: 'ui/manage/project/{0}/branch/{1}/validation_stamp/{2}'.format(project, branch, validationStamp),
+            successFn: function (summary) {
+                dialog.show({
+                    title: 'validation_stamp.update'.loc(),
+                    templateId: 'validation-stamp-update',
+                    initFn: function (config) {
+                        config.form.find('#validation-stamp-name').val(summary.name);
+                        config.form.find('#validation-stamp-description').val(summary.description);
+                    },
+                    submitFn: function (config) {
+                        ajax.put({
+                            url: 'ui/manage/project/{0}/branch/{1}/validation_stamp/{2}'.format(project, branch, validationStamp),
+                            data: {
+                                name: config.form.find('#validation-stamp-name').val(),
+                                description: config.form.find('#validation-stamp-description').val()
+                            },
+                            successFn: function (updatedValidationStamp) {
+                                config.closeFn();
+                                location.href = 'gui/project/{0}/branch/{1}/validation_stamp/{2}'.format(
+                                    updatedValidationStamp.branch.project.name,
+                                    updatedValidationStamp.branch.name,
+                                    updatedValidationStamp.name);
+                            },
+                            errorFn: ajax.simpleAjaxErrorFn(config.errorFn)
+                        });
+                    }
+                });
+            }
+        });
+    }
+
     // Adding a comment
-    $('#validation-stamp-comment-add').click(function () {
+    function addComment() {
         $('#validation-stamp-comment-form').show();
         $('#validation-stamp-comment').focus();
-    });
+    }
 
     // Cancelling the comment form
-    $('#validation-stamp-comment-cancel').click(function () {
+    function cancelComment() {
         $('#validation-stamp-comment-form').hide();
-    });
+    }
 
     // Sending the form
-    $('#validation-stamp-comment-form').submit(function () {
+    function sendComment() {
         ajax.post({
             url: 'ui/manage/project/{0}/branch/{1}/validation_stamp/{2}/comment'.format(
                 project,
@@ -37,6 +71,14 @@ define(['jquery', 'ajax', 'dialog', 'application', 'dynamic'], function ($, ajax
         });
         // OK
         return false;
-    });
+    }
+
+    $('#validation-stamp-update').click(updateValidationStamp);
+
+    $('#validation-stamp-comment-cancel').click(cancelComment);
+
+    $('#validation-stamp-comment-add').click(addComment);
+
+    $('#validation-stamp-comment-form').submit(sendComment);
 
 });
