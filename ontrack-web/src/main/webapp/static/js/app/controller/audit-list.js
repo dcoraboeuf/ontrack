63491ab8ce1d94da44d71fdca30ac5419ac86399
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-03 16:08:26 +0200

    requirejs: audit - no need for extra JS

--- ontrack-web/src/main/webapp/static/js/app/controller/audit-list.js
+++ ontrack-web/src/main/webapp/static/js/app/controller/audit-list.js
@@ -1,10 +1,67 @@
-define(['render'], function (render) {
+define(['render','ajax'], function (render, ajax) {
+
+    function subscribe (filter) {
+        ajax.get({
+            loading: {
+                el: '#subscription'
+            },
+            url: 'gui/event/subscribe?u=1' + filter,
+            successFn: function (data) {
+                $('#subscription').attr('src', 'static/images/unsubscription.png');
+                $('#subscription').attr('title', 'subscription.disable'.loc());
+                $('#subscription').unbind('click');
+                $('#subscription').click(function () {
+                    unsubscribe(filter);
+                });
+                // Notification
+                $('#subscription-notification-message').text('subscription.enabled'.loc());
+                $('#subscription-notification').show();
+            }
+        });
+    }
+
+    function unsubscribe (filter) {
+        ajax.get({
+            loading: {
+                el: '#subscription'
+            },
+            url: 'gui/event/unsubscribe?u=1' + filter,
+            successFn: function (data) {
+                $('#subscription').attr('src', 'static/images/subscription.png');
+                $('#subscription').attr('title', 'subscription.enable'.loc());
+                $('#subscription').unbind('click');
+                $('#subscription').click(function () {
+                    subscribe(filter);
+                });
+                // Notification
+                $('#subscription-notification-message').text('subscription.disabled'.loc());
+                $('#subscription-notification').show();
+            }
+        });
+    }
+
+    function init (filter) {
+        // Subscription item
+        var link = $('#subscription');
+        var subscribed = (link.attr('audit-subscribed') == 'true');
+        if (subscribed) {
+            link.click(function () {
+                unsubscribe(filter);
+            });
+        } else {
+            link.click(function () {
+                subscribe(filter);
+            });
+        }
+    }
 
     return {
         url: function (config) {
             return 'gui/event?u=1' + config.filter;
         },
-        render: render.asTableTemplate('audit')
+        render: render.asTableTemplate('audit', function (config) {
+            init(config.filter);
+        })
     }
 
 });
