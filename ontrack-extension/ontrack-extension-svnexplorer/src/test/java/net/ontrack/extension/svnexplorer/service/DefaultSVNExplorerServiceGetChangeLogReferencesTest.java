96e8c7ec24ff212a458f86e842389b9d87393481
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-04 13:23:57 +0200

    #167 Branching case

--- ontrack-extension/ontrack-extension-svnexplorer/src/test/java/net/ontrack/extension/svnexplorer/service/DefaultSVNExplorerServiceGetChangeLogReferencesTest.java
+++ ontrack-extension/ontrack-extension-svnexplorer/src/test/java/net/ontrack/extension/svnexplorer/service/DefaultSVNExplorerServiceGetChangeLogReferencesTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
@@ -82,6 +83,53 @@ public class DefaultSVNExplorerServiceGetChangeLogReferencesTest {
         assertEquals("/project/trunk", reference.getPath());
         assertEquals(110000, reference.getStart());
         assertEquals(100000, reference.getEnd());
+    }
+
+    @Test
+    public void branch_low() {
+        DefaultSVNExplorerService service = createService();
+        // Change log summary (same)
+        BranchSummary branch = new BranchSummary(1, "B1", "Branch 1", new ProjectSummary(1, "P1", "Project 1"));
+        ChangeLogSummary summary = new ChangeLogSummary(
+                "1",
+                branch,
+                new SVNBuild(
+                        new BuildSummary(101, "1.0.1", "Build 1.0.1", branch),
+                        new SVNHistory(ref("tags/11", 112))
+                                .add(ref("tags/1.0.1", 111))
+                                .add(ref("branches/1.0.x", 110))
+                                .add(ref("tags/1.0.0", 101))
+                                .add(ref("trunk", 100)),
+                        Collections.<BuildValidationStamp>emptyList(),
+                        Collections.<BuildPromotionLevel>emptyList()
+                ),
+                new SVNBuild(
+                        new BuildSummary(9, "0.0.9", "Build 0.0.9", branch),
+                        new SVNHistory(ref("tags/0.0.9", 91))
+                                .add(ref("tags/0.0.9", 91))
+                                .add(ref("trunk", 90)),
+                        Collections.<BuildValidationStamp>emptyList(),
+                        Collections.<BuildPromotionLevel>emptyList()
+                )
+        );
+        // References
+        Collection<ChangeLogReference> references = service.getChangeLogReferences(summary);
+        // Check
+        assertNotNull(references);
+        assertEquals(2, references.size());
+        Iterator<ChangeLogReference> iterator = references.iterator();
+        ChangeLogReference reference0 = iterator.next();
+        {
+            assertEquals("/project/trunk", reference0.getPath());
+            assertEquals(100, reference0.getStart());
+            assertEquals(90, reference0.getEnd());
+        }
+        ChangeLogReference reference1 = iterator.next();
+        {
+            assertEquals("/project/branches/1.0.x", reference1.getPath());
+            assertEquals(110, reference1.getStart());
+            assertEquals(101, reference1.getEnd());
+        }
     }
 
     private SVNReference ref(String path, int revision) {
