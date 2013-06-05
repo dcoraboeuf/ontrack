3ef8f9599347151b121f4209e3ce46e65f43b133
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-05 21:47:08 +0200

    #164 Actually deleting the builds

--- ontrack-backend/src/main/java/net/ontrack/backend/DefaultBuildCleanupService.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/DefaultBuildCleanupService.java
@@ -4,6 +4,7 @@ import net.ontrack.backend.dao.BuildCleanupDao;
 import net.ontrack.backend.dao.model.TBuildCleanup;
 import net.ontrack.core.model.BranchSummary;
 import net.ontrack.core.model.ProjectSummary;
+import net.ontrack.core.security.SecurityUtils;
 import net.ontrack.service.ManagementService;
 import net.ontrack.service.api.ScheduledService;
 import org.slf4j.Logger;
@@ -14,6 +15,7 @@ import org.springframework.scheduling.support.PeriodicTrigger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -23,11 +25,13 @@ public class DefaultBuildCleanupService implements ScheduledService, BuildCleanu
 
     private final BuildCleanupDao buildCleanupDao;
     private final ManagementService managementService;
+    private final SecurityUtils securityUtils;
 
     @Autowired
-    public DefaultBuildCleanupService(BuildCleanupDao buildCleanupDao, ManagementService managementService) {
+    public DefaultBuildCleanupService(BuildCleanupDao buildCleanupDao, ManagementService managementService, SecurityUtils securityUtils) {
         this.buildCleanupDao = buildCleanupDao;
         this.managementService = managementService;
+        this.securityUtils = securityUtils;
     }
 
     @Override
@@ -78,8 +82,19 @@ public class DefaultBuildCleanupService implements ScheduledService, BuildCleanu
         } else {
             logger.info("[build-cleanup] Retention={}, excluded promotion levels={}", conf.getRetention(), conf.getExcludedPromotionLevels());
             // Gets the list of builds to delete
-            List<Integer> buildIds = buildCleanupDao.getCandidatesForDeletion(branch, conf.getRetention(), conf.getExcludedPromotionLevels());
+            final List<Integer> buildIds = buildCleanupDao.getCandidatesForDeletion(branch, conf.getRetention(), conf.getExcludedPromotionLevels());
             logger.info("[build-cleanup] {} builds to delete", buildIds.size());
+            // Deleting the builds
+            securityUtils.asAdmin(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    for (int buildId : buildIds) {
+                        logger.info("[build-cleanup] Deleting build {}", buildId);
+                        managementService.deleteBuild(buildId);
+                    }
+                    return null;
+                }
+            });
         }
     }
 }
