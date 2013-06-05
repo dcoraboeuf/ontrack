bbb92fe76b324a43ceb04a4a8b69b4ae62d40a8b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-05 08:23:45 +0200

    #54 Button to remove a promotion from a build

--- ontrack-web/src/main/webapp/static/js/app/controller/build-promotion-levels.js
+++ ontrack-web/src/main/webapp/static/js/app/controller/build-promotion-levels.js
@@ -1,4 +1,18 @@
-define(['jquery','render'], function($, render) {
+define(['jquery', 'render', 'ajax', 'dynamic'], function ($, render, ajax, dynamic) {
+
+    function promotionLevelRemove(project, branch, build, promotionLevel) {
+        ajax.del({
+            url: 'ui/manage/project/{0}/branch/{1}/build/{2}/promotion_level/{3}'.format(
+                project,
+                branch,
+                build,
+                promotionLevel
+            ),
+            successFn: function () {
+                dynamic.reloadSection('build-promotion-levels');
+            }
+        });
+    }
 
     return {
         url: function (config) {
@@ -14,8 +28,19 @@ define(['jquery','render'], function($, render) {
                 return {
                     project: config.project,
                     branch: config.branch,
+                    build: config.build,
+                    admin: (config.admin == 'true'),
                     promotionLevels: promotionLevels
                 }
+            },
+            function (config) {
+                $('.promotion-level-remove').each(function (index, action) {
+                    var promotionLevel = $(action).attr('data-promotionlevel');
+                    $(action).unbind('click');
+                    $(action).click(function () {
+                        promotionLevelRemove(config.project, config.branch, config.build, promotionLevel);
+                    });
+                });
             })
     }
 
--- ontrack-web/src/main/webapp/static/js/lib/ajax.js
+++ ontrack-web/src/main/webapp/static/js/lib/ajax.js
@@ -1,4 +1,4 @@
-define(function () {
+define(['common'], function (common) {
 
     /**
      * Performs a PUT request
