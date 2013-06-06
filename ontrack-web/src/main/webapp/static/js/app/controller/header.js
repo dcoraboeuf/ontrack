a411a5813fdbb5028b3903e744c1b2d703257798
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-06 17:15:37 +0200

    #171 Correction of the language selection

--- ontrack-web/src/main/webapp/static/js/app/controller/header.js
+++ ontrack-web/src/main/webapp/static/js/app/controller/header.js
@@ -1,8 +1,28 @@
 define(['jquery'], function ($) {
 
-    $(document).ready(function () {
-        $('#header-signin').click(function () {
-            location.href = 'login?callbackUrl={0}'.format(encodeURIComponent(location.href));
+    function changeLanguage(lang) {
+        if (location.search.indexOf("language") > -1) {
+            location.search = location.search.replace(/language=[a-z][a-z]/, "language=" + lang);
+        } else if (location.search == "") {
+            var url = location.href;
+            if (url.substr(url.length - 1) == '?') {
+                location.href += "language=" + lang;
+            } else {
+                location.href += "?language=" + lang;
+            }
+        } else {
+            location.href += "&language=" + lang;
+        }
+    }
+
+    $('#header-signin').click(function () {
+        location.href = 'login?callbackUrl={0}'.format(encodeURIComponent(location.href));
+    });
+
+    $('.language-selection').each(function (index, action) {
+        var language = $(action).attr('data-language');
+        $(action).click(function () {
+            changeLanguage(language);
         });
     });
 
