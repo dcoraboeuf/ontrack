08bb1ddd272cb7bc19657c4317017be47d020146
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-08 22:00:47 +0200

    #137 Looping on the pages

---
+++ ontrack-web/src/main/webapp/static/js/app/dashboard.js
@@ -0,0 +1,27 @@
+define(['ajax'], function (ajax) {
+
+    // Context
+    var context = location.href.substring(location.href.indexOf('dashboard'));
+    // Initial index
+    var index = 0;
+
+    // Refresh function
+    function refresh () {
+        // Gets the URL
+        var url = 'ui/{0}/page/{1}'.format(context, index++);
+        // Call
+        ajax.get({
+            url: url,
+            loading: {
+                el: $('#dashboard-loading'),
+                mode: 'toggle'
+            },
+            errorFn: ajax.simpleAjaxErrorFn(ajax.elementErrorMessageFn($('#dashboard-error')))
+        })
+    }
+
+    // Refreshes the dashboard content
+    refresh();
+    setInterval(refresh, 10000);
+
+})
