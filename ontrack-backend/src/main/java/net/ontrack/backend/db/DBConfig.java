a22225d6b7c97b043d9f7bf622c31daa58538144
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-11 15:42:40 +0200

    #175 Branch/ValidationStamp dashboard table

--- ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 24;
+	public static final int VERSION = 25;
 	
 	@SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
