acffe8756978b4e4eca1a77c199aff4018a00494
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-13 17:20:28 +0200

    #172 Checkboxes

--- ontrack-web/src/main/webapp/static/js/app/controller/extensions.js
+++ ontrack-web/src/main/webapp/static/js/app/controller/extensions.js
@@ -1,8 +1,36 @@
-define(['render'], function (render) {
+define(['render', 'jquery'], function (render, $) {
+
+    function selectExtension(extension) {
+
+    }
+
+    function unselectExtension(extension) {
+
+    }
+
+    function setup(config) {
+        // Indexation
+        var extensionIndex = {};
+        $.each(extensions, function (index, extension) {
+            extensionIndex[extension.name] = extension;
+        });
+        // TODO Checkbox initialization
+        // Actions
+        $('.extension').each(function (index, input) {
+            var extension = $(input).attr('data-extension');
+            $(input).change(function () {
+                if ($(input).is(':checked')) {
+                    selectExtension(extension);
+                } else {
+                    unselectExtension(extension);
+                }
+            });
+        });
+    }
 
     return {
         url: 'ui/admin/extensions',
-        render: render.asTableTemplate('extensions')
+        render: render.asTableTemplate('extensions', setup)
     }
 
 })
