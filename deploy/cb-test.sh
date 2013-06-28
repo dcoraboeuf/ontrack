49ef6f62e273d8a301733aecd61c464ba47fb03d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-28 21:50:16 +0200

    #186 Optional creation of the application

--- deploy/cb-test.sh
+++ deploy/cb-test.sh
@@ -5,11 +5,12 @@ function show_help {
         echo "Ontrack Cloudbees deployment script."
 		echo ""
 		echo "Available options are:"
-		echo "  -h, --help              Displays this help"
-		echo "  -v, --version=version   Sets the ontrack version to deploy (required)"
-		echo "  -a, --appid=appid       ID of the application to create on Cloudbees (ontrack-test by default)"
-		echo "  -d, --database=dbname   ID of the database to create on Cloudbees (ontrack-test by default)."
-		echo "  -cd, --create-database  Creates the database from scratch (not done by default)"
+		echo "  -h, --help                 Displays this help"
+		echo "  -v, --version=version      Sets the ontrack version to deploy (required)"
+		echo "  -a, --appid=appid          ID of the application to create on Cloudbees (ontrack-test by default)"
+		echo "  -d, --database=dbname      ID of the database to create on Cloudbees (ontrack-test by default)."
+		echo "  -cd, --create-database     Creates the database from scratch (not done by default)"
+		echo "  -ca, --create-application  Creates the application from scratch (not done by default)"
 }
 
 # Input data
@@ -17,6 +18,7 @@ ONTRACK_DB=ontrack-test
 ONTRACK_APP=ontrack-test
 ONTRACK_VERSION=
 ONTRACK_DB_CREATE=no
+ONTRACK_APP_CREATE=no
 
 for i in "$@"
 do
@@ -40,6 +42,9 @@ do
 		-cd|--create-database)
 			ONTRACK_DB_CREATE=yes
 			;;
+		-ca|--create-application)
+			ONTRACK_APP_CREATE=yes
+			;;
 		*)
 			echo "Unknown option: $i"
 			show_help
@@ -57,10 +62,11 @@ then
 fi
 
 # Echo
-echo Ontrack version to deploy    : $ONTRACK_VERSION
-echo Ontrack CB application name  : $ONTRACK_APP
-echo Ontrack CB database name     : $ONTRACK_DB
-echo Ontrack CB database creation : $ONTRACK_DB_CREATE
+echo Ontrack version to deploy        : $ONTRACK_VERSION
+echo Ontrack CB application name      : $ONTRACK_APP
+echo Ontrack CB database name         : $ONTRACK_DB
+echo Ontrack CB database creation     : $ONTRACK_DB_CREATE
+echo Ontrack CB application  creation : $ONTRACK_APP_CREATE
 
 # Creating the database, deleting it if necessary
 if [ "$ONTRACK_DB_CREATE" == "yes" ]
@@ -87,38 +93,40 @@ then
 fi
 
 # Creating the application, deleting it if necessary
-bees app:list | grep $ONTRACK_APP
-if [ "$?" == "0" ]
+if [ "$ONTRACK_APP_CREATE" == "yes" ]
 then
-	echo Deleting application $ONTRACK_APP...
-	bees app:delete --force $ONTRACK_APP
+	bees app:list | grep $ONTRACK_APP
+	if [ "$?" == "0" ]
+	then
+		echo Deleting application $ONTRACK_APP...
+		bees app:delete --force $ONTRACK_APP
+		if [ "$?" != "0" ]
+		then
+			echo Could not delete the application.
+			exit 1
+		fi
+	fi
+	echo Creating application $ONTRACK_APP...
+	bees app:create --type tomcat $ONTRACK_APP
 	if [ "$?" != "0" ]
 	then
-		echo Could not delete the application.
+		echo Could not create the application.
 		exit 1
 	fi
+
+	echo Setting the JDK version to 1.7...
+	bees config:set --appid $ONTRACK_APP -R java_version=1.7
+
+	echo Setting the production profile...
+	bees config:set --appid $ONTRACK_APP -P spring.profiles.active=prod
+
+	echo Setting the home directory
+	bees config:set --appid $ONTRACK_APP -P ontrack.home=/private/ontrack/$ONTRACK_APP
+
+	echo Application $ONTRACK_APP has been created.
+
+	# Binding the database
+
+	bees app:bind --appid $ONTRACK_APP --database $ONTRACK_DB --alias ontrack
 fi
-echo Creating application $ONTRACK_APP...
-bees app:create --type tomcat $ONTRACK_APP
-if [ "$?" != "0" ]
-then
-	echo Could not create the application.
-	exit 1
-fi
-
-echo Setting the JDK version to 1.7...
-bees config:set --appid $ONTRACK_APP -R java_version=1.7
-
-echo Setting the production profile...
-bees config:set --appid $ONTRACK_APP -P spring.profiles.active=prod
-
-echo Setting the home directory
-bees config:set --appid $ONTRACK_APP -P ontrack.home=/private/ontrack/$ONTRACK_APP
-
-echo Application $ONTRACK_APP has been created.
-
-# Binding the database
-
-bees app:bind --appid $ONTRACK_APP --database $ONTRACK_DB --alias ontrack
-
 
