00c5ec690b83c9f2a0ca03add27afd9b3a22f977
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-28 20:50:13 +0200

    #186 Creation of a test application on CB

--- deploy/cb-test.sh
+++ deploy/cb-test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Main data
-ONTRACK_DB=ontrack-test
+# Input data
+ONTRACK=ontrack-test
+
+# Set-up data
+ONTRACK_DB=$ONTRACK
+ONTRACK_APP=$ONTRACK
 
 # Creating the database, deleting it if necessary
 bees db:list | grep $ONTRACK_DB
@@ -18,8 +22,29 @@ fi
 echo Creating database $ONTRACK_DB...
 bees db:create --username $ONTRACK_DB --password $ONTRACK_DB $ONTRACK_DB
 if [ "$?" != "0" ]
+then
+	echo Could not create the database.
+	exit 1
+fi
+echo Database $ONTRACK_DB has been created.
+
+# Creating the application, deleting it if necessary
+bees app:list | grep $ONTRACK_APP
+if [ "$?" == "0" ]
+then
+	echo Deleting application $ONTRACK_APP...
+	bees app:delete --force $ONTRACK_APP
+	if [ "$?" != "0" ]
 	then
-		echo Could not create the database.
+		echo Could not delete the application.
 		exit 1
 	fi
-echo Database $ONTRACK_DB has been created.
+fi
+echo Creating application $ONTRACK_APP...
+bees app:create --type tomcat $ONTRACK_APP
+if [ "$?" != "0" ]
+then
+	echo Could not create the application.
+	exit 1
+fi
+echo Application $ONTRACK_APP has been created.
