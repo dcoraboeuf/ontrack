826bc9e00cf79ad22d0056551e5e789589cf585f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-06-28 21:42:31 +0200

    #186 Options for the shell script

--- deploy/cb-test.sh
+++ deploy/cb-test.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 
-# Input data
-ONTRACK=ontrack-test
+# Help function
+function show_help {
+        echo "Ontrack Cloudbees deployment script."
+		echo ""
+		echo "Available options are:"
+		echo "  -h, --help              Displays this help"
+		echo "  -v, --version=version   Sets the ontrack version to deploy (required)"
+		echo "  -a, --appid=appid       ID of the application to create on Cloudbees (ontrack-test by default)"
+		echo "  -d, --database=dbname   ID of the database to create on Cloudbees (ontrack-test by default)."
+}
 
-# Set-up data
-ONTRACK_DB=$ONTRACK
-ONTRACK_APP=$ONTRACK
+# Input data
+ONTRACK_DB=ontrack-test
+ONTRACK_APP=ontrack-test
+ONTRACK_VERSION=
+
+for i in "$@"
+do
+	case $i in
+		-h|--help)
+			show_help
+			exit 0
+			;;
+		-v=*|--version=*)
+			ONTRACK_VERSION=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+			;;
+		-a=*|--appid=*)
+			ONTRACK_APP=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+			;;
+		-d=*|--database=*)
+			ONTRACK_DB=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+			;;
+		*)
+				# unknown option
+		;;
+	esac
+done
+
+# Checks
+if [ "$ONTRACK_VERSION" == "" ]
+then
+	echo Ontrack version -v is required.
+	show_help
+	exit 1
+fi
+
+# Echo
+echo Ontrack version to deploy   : $ONTRACK_VERSION
+echo Ontrack CB application name : $ONTRACK_APP
+echo Ontrack CB database name    : $ONTRACK_DB
 
 # Creating the database, deleting it if necessary
 bees db:list | grep $ONTRACK_DB
