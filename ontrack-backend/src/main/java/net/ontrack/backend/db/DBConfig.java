c298694956bfeb838ea2db1dbbfbf8b310a161a8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-07-11 17:02:20 +0200

    #81 `locale` column in the `accounts` table

--- ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/db/DBConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class DBConfig {
 	
-	public static final int VERSION = 27;
+	public static final int VERSION = 28;
 	
 	@SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
