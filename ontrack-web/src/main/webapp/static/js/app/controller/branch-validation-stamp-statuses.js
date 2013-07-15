e0aeb5cdf5691effe2602281fa12cdefb1ac0b7b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-07-15 20:47:24 +0200

    #203 Correct computation of size

--- ontrack-web/src/main/webapp/static/js/app/controller/branch-validation-stamp-statuses.js
+++ ontrack-web/src/main/webapp/static/js/app/controller/branch-validation-stamp-statuses.js
@@ -8,8 +8,18 @@ define(['flot'], function (flot) {
             )
         },
         render: function (container, append, config, data) {
-            var chartId = '{0}-chart'.format($(container).attr('id'));
-            var chart = $('<div id="{0}" style="width:800px;height:400px;background-color: yellow;"></div>'.format(chartId))
+            var id = config.id;
+            var chartId = '{0}-chart'.format(id);
+            var width = $('#{0}-title'.format(id)).width();
+            var height = Object.keys(data.table).length * 20;
+            var chart = $(
+                '<div id="{0}" style="width:{1}px;height:{2}px;background-color: yellow;"></div>'
+                    .format(
+                        chartId,
+                        width,
+                        height
+                    )
+            )
                 .appendTo(container);
             // FIXME Series
             // FIXME Options
