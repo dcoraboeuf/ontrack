b9075cfa2760d03ae3f5e0ee556fb8807d6fbf5c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-07-17 17:12:46 +0200

    hateoas: root

---
+++ ontrack-web/src/main/java/net/ontrack/web/hateoas/RootResourceController.java
@@ -0,0 +1,34 @@
+package net.ontrack.web.hateoas;
+
+import net.sf.jstring.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping("/rest")
+public class RootResourceController extends AbstractResourceController {
+
+    @Autowired
+    public RootResourceController(Strings strings) {
+        super(strings);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResourceSupport home() {
+        ResourceSupport resource = new ResourceSupport();
+        // Projects
+        resource.add(linkTo(methodOn(ProjectResourceController.class).projectList()).withRel("projects"));
+        // OK
+        return resource;
+    }
+
+}
