61041e098403358b52e52f98257131c264386e88
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-07-17 17:09:04 +0200

    hateoas: branch resources with a project

--- ontrack-web/src/main/java/net/ontrack/web/hateoas/ProjectResourceAssembler.java
+++ ontrack-web/src/main/java/net/ontrack/web/hateoas/ProjectResourceAssembler.java
@@ -4,6 +4,9 @@ import net.ontrack.core.model.ProjectSummary;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @Component
 public class ProjectResourceAssembler extends ResourceAssemblerSupport<ProjectSummary, ProjectResource> {
 
@@ -13,7 +16,9 @@ public class ProjectResourceAssembler extends ResourceAssemblerSupport<ProjectSu
 
     @Override
     public ProjectResource toResource(ProjectSummary entity) {
-        return createResourceWithId(entity.getId(), entity);
+        ProjectResource resource = createResourceWithId(entity.getId(), entity);
+        resource.add(linkTo(methodOn(ProjectResourceController.class).projectBranchList(entity.getId())).withRel("branches"));
+        return resource;
     }
 
     @Override
