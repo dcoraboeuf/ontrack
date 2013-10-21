de3e306ceb8b8bdb0622ac4f6001b7c1f1027fb7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-21 21:40:04 +0200

    #220 Export progress

--- ontrack-web/src/main/webapp/static/js/app/export.js
+++ ontrack-web/src/main/webapp/static/js/app/export.js
@@ -1,5 +1,29 @@
 define(['jquery', 'ajax'], function ($, ajax) {
 
+    function check(uid) {
+        ajax.get({
+            url: 'ui/manage/export/{0}/check'.format(uid),
+            successFn: function (ack) {
+                if (ack.success) {
+                    $('#project-export-loading').hide();
+                    // Download ready
+                    $('#project-export-ready').show();
+                    $('#project-export-link').attr(
+                        'href',
+                        'gui/project/export/{0}'.format(uid)
+                    )
+                } else {
+                    // Going on...
+                    window.setTimeout(check, 5000);
+                }
+            },
+            errorFn: ajax.simpleAjaxErrorFn(function (message) {
+                $('#project-export-loading').hide();
+                ajax.elementErrorMessageFn($('#project-export-error'))(message);
+            })
+        })
+    }
+
     $('#export-next').click(function () {
         var projects = $('.export-project-chk').find('input').filter(':checked').map(function (i, chk) {
             return $(chk).attr('data-project')
@@ -14,7 +38,12 @@ define(['jquery', 'ajax'], function ($, ajax) {
             },
             errorFn: ajax.simpleAjaxErrorFn(ajax.elementErrorMessageFn($('#export-error'))),
             successFn: function (response) {
-                console.log(response)
+                window.setTimeout(
+                    function () {
+                        check(response.uid)
+                    },
+                    5000
+                )
             }
         })
     })
