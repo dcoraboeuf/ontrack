6f2cf3538f073fa509e2eb3a285ce40e19128b0b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-21 21:17:45 +0200

    #220 Export button

--- ontrack-web/src/main/webapp/static/js/app/export.js
+++ ontrack-web/src/main/webapp/static/js/app/export.js
@@ -1,3 +1,19 @@
-define(['jquery'], function ($) {
+define(['jquery', 'ajax'], function ($, ajax) {
+
+    $('#export-next').click(function () {
+        var projects = $('.export-project-chk').find('input').filter(':checked').map(function (i, chk) {
+            return $(chk).attr('data-project')
+        }).toArray();
+        ajax.post({
+            url: 'ui/manage/export/project',
+            data: {
+                names: projects
+            },
+            loading: {
+                el: $('#export-next')
+            },
+            errorFn: ajax.simpleAjaxErrorFn(ajax.elementErrorMessageFn($('#export-error')))
+        })
+    })
 
 });
