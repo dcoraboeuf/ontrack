11a796a8ca8b1b21d9cf1e1bc5bf79ca1ab6472b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-25 16:16:13 +0200

    #254 Getting more details about the exception

--- ontrack-client/src/main/java/net/ontrack/client/support/ClientMessageException.java
+++ ontrack-client/src/main/java/net/ontrack/client/support/ClientMessageException.java
@@ -5,6 +5,11 @@ public class ClientMessageException extends ClientException {
 
     private final String message;
 
+    public ClientMessageException(Exception ex, String content) {
+        super(ex, content);
+        this.message = content;
+    }
+
     public ClientMessageException(String content) {
         super(content);
         this.message = content;
