70163e5da81f1d17f15e0525acc53e7b58662d60
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-29 21:19:12 +0100

    #222 Runas identity

--- ontrack-backend/src/main/java/net/ontrack/backend/security/RunAsAdminAuthentication.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/security/RunAsAdminAuthentication.java
@@ -1,13 +1,31 @@
 package net.ontrack.backend.security;
 
+import net.ontrack.core.model.Account;
 import net.ontrack.core.security.SecurityRoles;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 
+import java.util.Locale;
+
 public class RunAsAdminAuthentication extends AbstractAuthenticationToken {
+
+    private static final Account RUNAS_ACCOUNT = new Account(
+            -1,
+            "runas_admin",
+            "",
+            "",
+            SecurityRoles.ADMINISTRATOR,
+            "runas",
+            Locale.ENGLISH
+    );
 
     public RunAsAdminAuthentication() {
         super(AuthorityUtils.createAuthorityList(SecurityRoles.ADMINISTRATOR));
+    }
+
+    @Override
+    public Account getDetails() {
+        return RUNAS_ACCOUNT;
     }
 
     @Override
