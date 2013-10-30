6d0ed26605cbeae22b21b57859d383ac7f8719c6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-30 10:55:28 +0100

    #222 Global ACL - client call

--- ontrack-web/src/main/webapp/static/js/app/acl-global.js
+++ ontrack-web/src/main/webapp/static/js/app/acl-global.js
@@ -1,5 +1,14 @@
 define(['jquery', 'ajax', 'app/component/account-role-selection'], function ($, ajax, accountRoleSelection) {
 
+    function addACL(config, id, fn) {
+        ajax.put({
+            url: 'ui/admin/acl/global/{0}/{1}'.format(id, fn),
+            successFn: function () {
+                // TODO Refreshes the list of ACLs
+            }
+        })
+    }
+
     // Gets the list of global functions
     ajax.get({
         url: 'ui/admin/acl/global',
@@ -8,7 +17,8 @@ define(['jquery', 'ajax', 'app/component/account-role-selection'], function ($, 
             accountRoleSelection.init(
                 $('#acl-account-form'),
                 {
-                    roles: fns
+                    roles: fns,
+                    submitFn: addACL
                 }
             )
         }
