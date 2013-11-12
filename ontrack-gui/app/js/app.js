13eabba95969708bf225454aacb9d7cfd825bf0e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-12 20:39:05 +0100

    2.0: Replaced `view1` by `home`.

--- ontrack-gui/app/js/app.js
+++ ontrack-gui/app/js/app.js
@@ -12,7 +12,7 @@ angular.module('ontrackApp', [
         'ontrackApp.controllers'
     ]).
     config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
         $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.otherwise({redirectTo: '/home'});
     }]);
