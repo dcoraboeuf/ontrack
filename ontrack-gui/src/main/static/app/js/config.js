da8306db5300e3f8e43e5c39f71149ce82a6cb23
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-12 20:17:27 +0100

    2.0: Reverting to AngularJS Seed

--- ontrack-gui/src/main/static/app/js/config.js
+++
@@ -1,6 +0,0 @@
-angular.module('config', []).value(
-    'config',
-    {
-        server: 'http://localhost:8080/ontrack'
-    }
-);
--- ontrack-gui/src/main/static/app/js/controllers.js
+++
@@ -1,9 +0,0 @@
-var ontrackApp = angular.module('ontrackApp', ['config']);
-
-ontrackApp.controller('ProjectListCtrl', ['$scope', 'config', '$http', function ($scope, config, $http) {
-    $http
-        .get(config.server + '/ui/manage/project')
-        .success(function (data) {
-            $scope.projects = data;
-        })
-}]);
