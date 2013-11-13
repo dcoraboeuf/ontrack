e4940ac6b0a0396d3ede7fa88cadd0dc01d32d38
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-13 13:36:54 +0100

    2.0: Using a `ot-page-close` directive for the close button

--- ontrack-gui/app/js/directives.js
+++ ontrack-gui/app/js/directives.js
@@ -3,9 +3,18 @@
 /* Directives */
 
 
-angular.module('ontrackApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+var otDirectives = angular.module('ontrackApp.directives', []);
+otDirectives.directive('appVersion', ['version', function (version) {
+    return function (scope, elm, attrs) {
+        elm.text(version);
     };
-  }]);
+}]);
+otDirectives.directive('otPageClose', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/page-close.html',
+        scope: {
+            href: '@href'
+        }
+    };
+});
