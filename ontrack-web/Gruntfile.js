ced3336fdade0f86037440936daf83c6421a9973
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-19 21:19:40 +0100

    2.0: `watch` and `delta` tasks

--- ontrack-web/Gruntfile.js
+++ ontrack-web/Gruntfile.js
@@ -187,11 +187,144 @@ module.exports = function ( grunt ) {
                     '<%= recess.compile.dest %>'
                 ]
             }
+        },
+
+        /**
+         * And for rapid development, we have a watch set up that checks to see if
+         * any of the files listed below change, and then to execute the listed
+         * tasks when they do. This just saves us from having to type "grunt" into
+         * the command-line every time we want to see what we're working on; we can
+         * instead just leave "grunt watch" running in a background terminal. Set it
+         * and forget it, as Ron Popeil used to tell us.
+         *
+         * But we don't need the same thing to happen for all the files.
+         */
+        delta: {
+            /**
+             * By default, we want the Live Reload to work for all tasks; this is
+             * overridden in some tasks (like this file) where browser resources are
+             * unaffected. It runs by default on port 35729, which your browser
+             * plugin should auto-detect.
+             */
+            options: {
+                livereload: true
+            },
+
+            /**
+             * When the Gruntfile changes, we just want to lint it. In fact, when
+             * your Gruntfile changes, it will automatically be reloaded!
+             */
+            gruntfile: {
+                files: 'Gruntfile.js',
+                tasks: [ 'jshint:gruntfile' ],
+                options: {
+                    livereload: false
+                }
+            },
+
+            /**
+             * When our JavaScript source files change, we want to run lint them and
+             * run our unit tests.
+             */
+            jssrc: {
+                files: [
+                    '<%= src_dir %>/<%= app_files.js %>'
+                ],
+                tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs' ]
+            },
+
+            /**
+             * When our CoffeeScript source files change, we want to run lint them and
+             * run our unit tests.
+             */
+                /*
+            coffeesrc: {
+                files: [
+                    '<%= app_files.coffee %>'
+                ],
+                tasks: [ 'coffeelint:src', 'coffee:source', 'karma:unit:run', 'copy:build_appjs' ]
+            },*/
+
+            /**
+             * When assets are changed, copy them. Note that this will *not* copy new
+             * files, so this is probably not very useful.
+             */
+            assets: {
+                files: [
+                    'src/assets/**/*'
+                ],
+                tasks: [ 'copy:build_assets' ]
+            },
+
+            /**
+             * When index.html changes, we need to compile it.
+             */
+            html: {
+                files: [ '<%= src_dir %>/<%= app_files.html %>' ],
+                tasks: [ 'index:build' ]
+            },
+
+            /**
+             * When our templates change, we only rewrite the template cache.
+             */
+            tpls: {
+                files: [
+                    '<%= src_dir %>/<%= app_files.tpl %>'
+                ],
+                tasks: [ 'copy:build_apptpl' ]
+            },
+
+            /**
+             * When the CSS files change, we need to compile and minify them.
+             */
+            less: {
+                files: [ 'src/**/*.less' ],
+                tasks: [ 'recess:build' ]
+            },
+
+            /**
+             * When a JavaScript unit test file changes, we only want to lint it and
+             * run the unit tests. We don't want to do any live reloading.
+             */
+            jsunit: {
+                files: [
+                    '<%= app_files.jsunit %>'
+                ],
+                tasks: [ 'jshint:test', 'karma:unit:run' ],
+                options: {
+                    livereload: false
+                }
+            },
+
+            /**
+             * When a CoffeeScript unit test file changes, we only want to lint it and
+             * run the unit tests. We don't want to do any live reloading.
+             */
+            coffeeunit: {
+                files: [
+                    '<%= app_files.coffeeunit %>'
+                ],
+                tasks: [ 'coffeelint:test', 'karma:unit:run' ],
+                options: {
+                    livereload: false
+                }
+            }
         }
 
     };
 
     grunt.initConfig( grunt.util._.extend( taskConfig, userConfig ) );
+
+    /**
+     * In order to make it safe to just compile or copy *only* what was changed,
+     * we need to ensure we are starting from a clean, fresh build. So we rename
+     * the `watch` task to `delta` (that's why the configuration var above is
+     * `delta`) and then add a new task called `watch` that does a clean build
+     * before watching for changes.
+     */
+    grunt.renameTask( 'watch', 'delta' );
+    grunt.registerTask( 'watch', [ 'build', 'delta' ] );
+    // grunt.registerTask( 'watch', [ 'build', 'karma:unit', 'delta' ] );
 
     /**
      * The default task is to build and compile.
