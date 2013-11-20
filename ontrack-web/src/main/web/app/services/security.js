36a95d34ef025416e1b55f332d88d3dd4436072f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-20 12:37:17 +0100

    2.0: Authentication service

--- ontrack-web/src/main/web/app/services/security.js
+++ ontrack-web/src/main/web/app/services/security.js
@@ -1,33 +1,36 @@
 angular.module('ontrack.service.security', ['ontrack.config', 'ontrack.service.core'])
     .service('securityService', function securityService($http, config, errorService) {
-        return {
-            // Properties
-            user: undefined,
-            // Methods
-            authenticate: function authenticate(name, password, callbackFn, errorMessageFn) {
-                $http
-                    .get(
-                    config.api('auth/authenticate'),
-                    {
-                        headers: {
-                            'Authorization': 'Basic ' + btoa(name + ':' + password)
-                        }
-                    })
-                    .success(function (user) {
-                        this.authenticationOk(user);
-                        callbackFn(user);
-                    })
-                    .error(function (text, status) {
-                        errorMessageFn(
-                            status == 403
-                                ? 'Username and/or password incorrect'
-                                : errorService.errorMsg(text, status)
-                        )
-                    })
-            },
-            authenticationOk: function authenticationOk(authentication) {
-                this.user = authentication;
-            }
-        }
+
+        var self = {
+            user: undefined
+        };
+
+        self.authenticationOk = function (user) {
+            self.user = user;
+        };
+
+        self.authenticate = function (name, password, callbackFn, errorMessageFn) {
+            $http
+                .get(
+                config.api('auth/authenticate'),
+                {
+                    headers: {
+                        'Authorization': 'Basic ' + btoa(name + ':' + password)
+                    }
+                })
+                .success(function (user) {
+                    self.authenticationOk(user);
+                    callbackFn(user);
+                })
+                .error(function (text, status) {
+                    errorMessageFn(
+                        status == 403
+                            ? 'Username and/or password incorrect'
+                            : errorService.errorMsg(text, status)
+                    )
+                })
+        };
+
+        return self;
     })
 ;
