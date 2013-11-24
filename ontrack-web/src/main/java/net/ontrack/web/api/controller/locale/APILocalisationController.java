7d1a6b96926abe1e246dcd1582d43aeb6210d0ad
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-24 21:07:28 +0100

    2.0:The localization controllers generate a JSON map

--- ontrack-web/src/main/java/net/ontrack/web/api/controller/locale/APILocalisationController.java
+++ ontrack-web/src/main/java/net/ontrack/web/api/controller/locale/APILocalisationController.java
@@ -1,65 +1,29 @@
 package net.ontrack.web.api.controller.locale;
 
 import net.sf.jstring.Strings;
+import org.springframework.http.ResponseEntity;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
 
-import static org.apache.commons.lang3.StringEscapeUtils.escapeEcmaScript;
-import static org.apache.commons.lang3.StringUtils.replace;
-
 public abstract class APILocalisationController {
 
-	protected final Strings strings;
+    protected final Strings strings;
 
-	public APILocalisationController(Strings strings) {
-		this.strings = strings;
-	}
-	
-	public abstract void localisation(Locale locale, HttpServletResponse response, String language, String version) throws IOException;
+    public APILocalisationController(Strings strings) {
+        this.strings = strings;
+    }
 
-	protected String generateJS(Locale locale) {
-		// Restricts the locale
-		locale = strings.getSupportedLocales().filterForLookup(locale);
-		
-		// Gets the list of key/values
-		Map<String, String> map = strings.getKeyValues(locale);
-		// Output
-		StringBuilder js = new StringBuilder();
-		js.append("// ").append(new Date()).append("\n");
-		js.append("var l = {\n");
-		int i = 0;
-		for (Map.Entry<String, String> entry : map.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			if (i > 0) {
-				js.append(",\n");
-			}
-			js.append(String.format("'%s': '%s'", key, escape(value)));
-			i++;
-		}
-		js.append("\n};\n");
-		// Content
-		String content = js.toString();
-		return content;
-	}
+    public abstract ResponseEntity<Map<String, String>> localisation(String language, String version) throws IOException;
 
-	protected String escape(String value) {
-		return escapeEcmaScript(replace(value, "''", "'"));
-	}
+    protected Map<String, String> generateLocalizationMap(Locale locale) {
+        // Restricts the locale
+        locale = strings.getSupportedLocales().filterForLookup(locale);
 
-	protected void writeJS(String content, HttpServletResponse response) throws UnsupportedEncodingException, IOException {
-		byte[] bytes = content.getBytes("UTF-8");
-		response.setContentType("text/javascript");
-		response.setContentLength(bytes.length);
-		ServletOutputStream outputStream = response.getOutputStream();
-		outputStream.write(bytes);
-		outputStream.flush();
-	}
+        // Gets the list of key/values
+        return strings.getKeyValues(locale);
+    }
+
 
 }
--- ontrack-web/src/main/java/net/ontrack/web/api/controller/locale/ProductionAPILocalisationController.java
+++ ontrack-web/src/main/java/net/ontrack/web/api/controller/locale/ProductionAPILocalisationController.java
@@ -4,13 +4,14 @@ import net.ontrack.core.RunProfile;
 import net.sf.jstring.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Locale;
@@ -20,57 +21,58 @@ import java.util.concurrent.ConcurrentHashMap;
 @Controller
 @Profile(RunProfile.PROD)
 public class ProductionAPILocalisationController extends APILocalisationController {
-	
-	private static final long CACHE_PERIOD_SECONDS = 365 * 24 * 3600L;
-	
-	private final Map<Locale, String> contents = new ConcurrentHashMap<>();
 
-	@Autowired
-	public ProductionAPILocalisationController(Strings strings) {
-		super(strings);
-	}
-	
-	/**
-	 * Preloads all the versions
-	 */
-	@PostConstruct
-	public void init() {
-		// Gets all the locales
-		Collection<Locale> locales = strings.getSupportedLocales().getSupportedLocales();
-		for (Locale locale : locales) {
-			contents.put(locale, generateJS(locale));
-		}
-	}
+    private static final long CACHE_PERIOD_SECONDS = 365 * 24 * 3600L;
 
-	/**
-	 * All contents have already been generated at start-up. They are served
-	 * only once and cache parameters are set to the response.
-	 * 
-	 * @param version
-	 *            This parameter prevents the caching from one version to the
-	 *            other
-	 */
-	@Override
-	@RequestMapping(value = "/api/localization/{language}/{version:.*}", method = RequestMethod.GET)
-	public void localisation(Locale locale, HttpServletResponse response, @PathVariable String language, @PathVariable String version) throws IOException {
-		
-		if (locale == null) {
-			locale = Locale.ENGLISH;
-		}
-		
-		locale = strings.getSupportedLocales().filterForLookup(locale);
-		
-		String content = contents.get(locale);
-		if (content == null) {
-			throw new IOException("Could not find any content for locale " + locale);
-		}
-		
-		// See https://developers.google.com/speed/docs/best-practices/caching for cache management
-		final long now = System.currentTimeMillis();
-		response.setDateHeader("Expires", now + CACHE_PERIOD_SECONDS * 1000L);
-		response.setDateHeader("Last-Modified", now - 3600 * 1000);
-		
-		writeJS(content, response);
-	}
+    private final Map<Locale, Map<String, String>> contents = new ConcurrentHashMap<>();
+
+    @Autowired
+    public ProductionAPILocalisationController(Strings strings) {
+        super(strings);
+    }
+
+    /**
+     * Preloads all the versions
+     */
+    @PostConstruct
+    public void init() {
+        // Gets all the locales
+        Collection<Locale> locales = strings.getSupportedLocales().getSupportedLocales();
+        for (Locale locale : locales) {
+            contents.put(locale, generateLocalizationMap(locale));
+        }
+    }
+
+    /**
+     * All contents have already been generated at start-up. They are served
+     * only once and cache parameters are set to the response.
+     *
+     * @param version This parameter prevents the caching from one version to the
+     *                other
+     */
+    @Override
+    @RequestMapping(value = "/api/localization/{language}/{version:.*}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, String>> localisation(String language, String version) throws IOException {
+        Locale locale = new Locale(language);
+        locale = strings.getSupportedLocales().filterForLookup(locale);
+
+        Map<String, String> content = contents.get(locale);
+        if (content == null) {
+            throw new IOException("Could not find any content for locale " + locale);
+        }
+
+        // See https://developers.google.com/speed/docs/best-practices/caching for cache management
+        final long now = System.currentTimeMillis();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setExpires(now + CACHE_PERIOD_SECONDS * 1000L);
+        headers.setLastModified(now - 3600 * 1000);
+
+        // OK
+        return new ResponseEntity<>(
+                content,
+                headers,
+                HttpStatus.OK
+        );
+    }
 
 }
