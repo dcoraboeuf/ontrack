91a156956cd7eb4580690d678ba755cc11784a54
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-26 20:02:45 +0100

    2.0: defining the breadcrumbs for the project page

--- ontrack-web/src/main/web/app/project/project-page.js
+++ ontrack-web/src/main/web/app/project/project-page.js
@@ -21,9 +21,16 @@ angular.module('ontrack.project.page', [
     .controller('ProjectCtrl', function ProjectCtrl($scope, $state, $stateParams, $translate, projectService, messageService, dialog) {
         // Loads the project
         projectService.getProject($stateParams.projectName, function (projectResource) {
+            console.log('Getting project back.');
             // Page definition
             $scope.page = {
                 title: projectResource.name,
+                breadcrumbs: [
+                    {
+                        text: $translate('home'),
+                        link: '/home'
+                    }
+                ],
                 description: projectResource.description,
                 commands: [
                     {
