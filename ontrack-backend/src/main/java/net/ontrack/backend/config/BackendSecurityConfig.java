3f5ec7c8b961a45aa25a6fd47160e7e402e23cc3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-27 10:49:18 +0100

    #285 Backend security configuration

---
+++ ontrack-backend/src/main/java/net/ontrack/backend/config/BackendSecurityConfig.java
@@ -0,0 +1,48 @@
+package net.ontrack.backend.config;
+
+import net.ontrack.core.RunProfile;
+import net.ontrack.core.support.MapBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.access.intercept.aopalliance.MethodSecurityInterceptor;
+import org.springframework.security.access.method.MapBasedMethodSecurityMetadataSource;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Profile({RunProfile.PROD, RunProfile.IT, RunProfile.DEV})
+@EnableGlobalMethodSecurity
+public class BackendSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+    @Autowired
+    private AccessDecisionManager accessDecisionManager;
+
+    @Override
+    protected AccessDecisionManager accessDecisionManager() {
+        return accessDecisionManager;
+    }
+
+    @Bean
+    public MethodSecurityInterceptor methodSecurityInterceptor() {
+        MethodSecurityInterceptor interceptor = new MethodSecurityInterceptor();
+        interceptor.setAccessDecisionManager(accessDecisionManager);
+        interceptor.setSecurityMetadataSource(
+                new MapBasedMethodSecurityMetadataSource(
+                        MapBuilder.<String, List<ConfigAttribute>>create()
+                                .with("execution(@net.ontrack.core.security.ProjectGrant * net.ontrack.backend.*.*(..))", Arrays.<ConfigAttribute>asList(new SecurityConfig("project")))
+                                .with("execution(@net.ontrack.core.security.GlobalGrant * net.ontrack.backend.*.*(..))", Arrays.<ConfigAttribute>asList(new SecurityConfig("global")))
+                                .get()
+                )
+        );
+        return interceptor;
+    }
+
+}
