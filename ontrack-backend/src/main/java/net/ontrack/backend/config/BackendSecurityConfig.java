a0094dc4db8442363b925d7c21f4efdf90730a98
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-11-29 14:51:40 +0100

    #285 Switching back to XML configuration for the back-end security layer

--- ontrack-backend/src/main/java/net/ontrack/backend/config/BackendSecurityConfig.java
+++ ontrack-backend/src/main/java/net/ontrack/backend/config/BackendSecurityConfig.java
@@ -1,67 +1,9 @@
 package net.ontrack.backend.config;
 
-import net.ontrack.core.support.MapBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.ConfigAttribute;
-import org.springframework.security.access.SecurityConfig;
-import org.springframework.security.access.method.MapBasedMethodSecurityMetadataSource;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@EnableGlobalMethodSecurity
-public class BackendSecurityConfig extends GlobalMethodSecurityConfiguration {
-
-    private final MapBasedMethodSecurityMetadataSource mapBasedMethodSecurityMetadataSource = new MapBasedMethodSecurityMetadataSource();
-
-    @Autowired
-    private AccessDecisionManager accessDecisionManager;
-
-    @Autowired
-    private List<AuthenticationProvider> authenticationProviderList;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        for (AuthenticationProvider authenticationProvider : authenticationProviderList) {
-            auth.authenticationProvider(authenticationProvider);
-        }
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-    @Override
-    protected AccessDecisionManager accessDecisionManager() {
-        return accessDecisionManager;
-    }
-
-    @Override
-    protected MapBasedMethodSecurityMetadataSource customMethodSecurityMetadataSource() {
-        return mapBasedMethodSecurityMetadataSource;
-    }
-
-    @Bean
-    public BeanPostProcessor protectPointcutPostProcessor() {
-        BackendProtectPointcutPostProcessor processor = new BackendProtectPointcutPostProcessor(mapBasedMethodSecurityMetadataSource);
-        processor.setPointcutMap(
-                MapBuilder.<String, List<ConfigAttribute>>create()
-                        .with("execution(@net.ontrack.core.security.ProjectGrant * net.ontrack.backend.*.*(..))", Arrays.<ConfigAttribute>asList(new SecurityConfig("project")))
-                        .with("execution(@net.ontrack.core.security.GlobalGrant * net.ontrack.backend.*.*(..))", Arrays.<ConfigAttribute>asList(new SecurityConfig("global")))
-                        .get()
-        );
-        return processor;
-    }
+@ImportResource("classpath:META-INF/spring/backend-security.xml")
+public class BackendSecurityConfig {
 }
