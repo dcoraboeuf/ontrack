2f65c7fa3bb10859c41fcb71f71f13fcffed5e51
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-02-09 20:23:01 +0100

    #307 JIRA configuration list display

--- ontrack-extension/ontrack-extension-jira/src/main/resources/META-INF/extension/jira-configuration-list.js
+++ ontrack-extension/ontrack-extension-jira/src/main/resources/META-INF/extension/jira-configuration-list.js
@@ -1,7 +1,8 @@
-define([], function () {
+define(['render'], function (render) {
 
     return {
-        url: 'ui/extension/jira/configuration'
+        url: 'ui/extension/jira/configuration',
+        render: render.asTableTemplate('extension/jira-configuration-list-row')
     }
 
 });
