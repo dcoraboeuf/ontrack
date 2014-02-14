828a42a277632483afedf8d117c4d9cce7c52913
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-02-14 20:32:04 +0100

    #306 Clean-up of the SVN extension

--- ontrack-extension/ontrack-extension-svn/src/main/java/net/ontrack/extension/svn/SubversionExtension.java
+++ ontrack-extension/ontrack-extension-svn/src/main/java/net/ontrack/extension/svn/SubversionExtension.java
@@ -1,7 +1,6 @@
 package net.ontrack.extension.svn;
 
 import net.ontrack.extension.api.action.TopActionExtension;
-import net.ontrack.extension.api.configuration.ConfigurationExtension;
 import net.ontrack.extension.api.property.PropertyExtensionDescriptor;
 import net.ontrack.extension.api.support.ExtensionAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,24 +17,16 @@ public class SubversionExtension extends ExtensionAdapter {
     public static final String SUBVERSION_BUILD_PATH = "buildPath";
     private final SubversionPathPropertyExtension subversionPathPropertyExtension;
     private final SubversionBuildPathPropertyExtension subversionBuildPathPropertyExtension;
-    private final SubversionConfigurationExtension subversionConfigurationExtension;
-    private final IndexationConfigurationExtension indexationConfigurationExtension;
-    private final IndexationActionController indexationActionController;
     private final SVNConfigurationController svnConfigurationController;
 
     @Autowired
     public SubversionExtension(
             SubversionPathPropertyExtension subversionPathPropertyExtension,
             SubversionBuildPathPropertyExtension subversionBuildPathPropertyExtension,
-            SubversionConfigurationExtension subversionConfigurationExtension,
-            IndexationConfigurationExtension indexationConfigurationExtension,
-            IndexationActionController indexationActionController, SVNConfigurationController svnConfigurationController) {
+            SVNConfigurationController svnConfigurationController) {
         super(EXTENSION);
         this.subversionPathPropertyExtension = subversionPathPropertyExtension;
         this.subversionBuildPathPropertyExtension = subversionBuildPathPropertyExtension;
-        this.subversionConfigurationExtension = subversionConfigurationExtension;
-        this.indexationConfigurationExtension = indexationConfigurationExtension;
-        this.indexationActionController = indexationActionController;
         this.svnConfigurationController = svnConfigurationController;
     }
 
@@ -45,14 +36,7 @@ public class SubversionExtension extends ExtensionAdapter {
     }
 
     @Override
-    public List<? extends ConfigurationExtension> getConfigurationExtensions() {
-        return Arrays.asList(subversionConfigurationExtension, indexationConfigurationExtension);
-    }
-
-    @Override
     public Collection<? extends TopActionExtension> getTopLevelActions() {
-        return Arrays.asList(
-                indexationActionController,
-                svnConfigurationController);
+        return Arrays.asList(svnConfigurationController);
     }
 }
