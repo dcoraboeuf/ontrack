ab056366be60219e7ce5c80859b036b99e287f0d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-02-25 17:46:55 +0100

    #306 Indexation of a range

--- ontrack-extension/ontrack-extension-svn/src/main/resources/META-INF/extension/subversion-configuration-list.js
+++ ontrack-extension/ontrack-extension-svn/src/main/resources/META-INF/extension/subversion-configuration-list.js
@@ -31,7 +31,22 @@ define(['jquery', 'crud', 'ajax', 'dialog', 'common'], function ($, crud, ajax, 
                                 errorFn: ajax.simpleAjaxErrorFn(dialog.errorFn)
                             })
                         });
-                        // TODO Range indexation
+                        // Range indexation
+                        dialog.form.find('#subversion-indexation-dialog-range-submit').click(function () {
+                            var from = dialog.form.find('#subversion-indexation-dialog-range-from').val();
+                            var to = dialog.form.find('#subversion-indexation-dialog-range-to').val();
+                            ajax.post({
+                                url: 'ui/extension/svn/indexation/{0}/range?from={1}&to={2}'.format(repositoryId, from, to),
+                                successFn: function (ack) {
+                                    if (ack.success) {
+                                        dialog.closeFn()
+                                    } else {
+                                        dialog.errorFn('subversion.indexation.alreadyrunning'.loc())
+                                    }
+                                },
+                                errorFn: ajax.simpleAjaxErrorFn(dialog.errorFn)
+                            })
+                        });
                         // Full indexation
                         dialog.form.find('#subversion-indexation-dialog-full-submit').click(function () {
                             common.confirmAndCall(
