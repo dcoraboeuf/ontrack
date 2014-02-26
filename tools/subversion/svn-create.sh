71204b54501a34254454bb19c5835cad0c3b33dc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-02-26 10:07:42 +0100

    #306 Tools to create a SVN server that can be used for tests

---
+++ tools/subversion/svn-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Repository root
+ROOT=`pwd`/repos
+rm -rf $ROOT
+echo Creating SVN repository at $ROOT
+svnadmin create $ROOT
+
+# Starting the repository
+svnserve --daemon --root $ROOT --pid-file `pwd`/svn.pid
+PID=`cat svn.pid`
+echo SVN server started with PID $PID
+
